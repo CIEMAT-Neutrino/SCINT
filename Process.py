3f23858a06ba7fa9adf571c0b7a2c133d3340f52
mantheys <sergio.manthey@gmail.com>
2022-10-24 15:50:42 +0200

    PREPARE_PROCESS_SCRIPT

--- Process.py
+++ Process.py
@@ -2,14 +2,14 @@ from lib.my_functions import load_npy
 from lib.my_functions import compute_pedestal_variables
 from lib.my_functions import compute_peak_variables
 
-N_runs     =["2","10","22","26"]     
+N_runs     =[10,22,26]     
 N_channels =[0,1,4,6]       
 Pl         =[-1,-1,-1,-1]   #polarity
 P_channels ={}
 for ch,pl in zip(N_channels,Pl): P_channels[ch]=pl
 
 L_channels  =["SiPM1","SiPM2","PMT","SuperCell"]
-RUNS=load_npy(N_runs, N_channels,P_channels)
+RUNS=load_npy(N_runs, N_channels,P_channels,"data/")
 
 #Customizable
 PROP={};
