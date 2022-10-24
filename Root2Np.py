3378b1f79114e7a63900dd0b578053fa9596e4e6
mantheys <sergio.manthey@gmail.com>
2022-10-24 21:35:43 +0200

    SCRIPT_ROOT_2_NUMPY

---
+++ Root2Np.py
@@ -0,0 +1,11 @@
+from lib.my_functions import root2npy
+
+RUNS=[10,22,26]
+RUNS_calib=["02"]
+
+CH=[0,1,4,6]
+CH_calib=[0,1,6] #4 is the PMT
+
+for run in RUNS:
+    for ch in CH:
+        root2npy("data/run"+str(run).zfill(2)+"_ch"+str(ch)+".root","data/run"+str(run).zfill(2)+"_ch"+str(ch)+".npy")
