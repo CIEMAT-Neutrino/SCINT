8cefade2ee7210276b532f6d9586b9b7204a706e
mantheys <sergio.manthey@gmail.com>
2022-10-24 14:34:14 +0200

    SOME_UPDATES

---
+++ Process.py
@@ -0,0 +1,25 @@
+from lib.my_functions import load_npy
+from lib.my_functions import compute_pedestal_variables
+from lib.my_functions import compute_peak_variables
+
+N_runs     =["10","22"]     
+N_channels =[0,1,4,6]       
+Pl         =[-1,-1,-1,-1]   #polarity
+P_channels ={}
+for ch,pl in zip(N_channels,Pl): P_channels[ch]=pl
+
+L_channels  =["SiPM1","SiPM2","PMT","SuperCell"]
+RUNS=load_npy(N_runs, N_channels,P_channels)
+
+#Customizable
+PROP={};
+PROP["sampling"]=4; #ns
+PROP["NBins_Ped"]=250;
+
+# RUNS.keys()
+
+compute_pedestal_variables(RUNS,PROP["NBins_Ped"])
+# RUNS["10"][0].keys()
+
+compute_peak_variables(RUNS)
+# RUNS["10"][0].keys()
