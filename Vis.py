c5a8a47457b384ed726190a6332526a598ef644b
mantheys <sergio.manthey@gmail.com>
2022-10-24 17:23:21 +0200

    UPDATED_VIS_AND_LIBRARY

--- Vis.py
+++ Vis.py
@@ -1,4 +1,9 @@
 # cd to /lib and run -> python3 Vis.py
-from lib.my_functions import vis_raw_npy
+from lib.vis_functions import vis_raw_npy
 
-vis_raw_npy([10],[0]) # Input variables should be lists of integers
+RUNS = [10]
+CH   = [0,1,4,6]
+POL  = [1,1,1,1]
+PATH = "data/"
+
+vis_raw_npy(RUNS,CH,POL,PATH) # Input variables should be lists of integers
