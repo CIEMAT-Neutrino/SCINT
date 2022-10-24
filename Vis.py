3089f3b30450951bd9127dda1ead000ad7de017f
mantheys <sergio.manthey@gmail.com>
2022-10-24 17:49:40 +0200

    SOME_UPDATES

--- Vis.py
+++ Vis.py
@@ -1,9 +1,13 @@
 # cd to /lib and run -> python3 Vis.py
 from lib.vis_functions import vis_raw_npy
 
+PATH = "data/"
+
 RUNS = [10]
 CH   = [0,1,4,6]
 POL  = [1,1,1,1]
-PATH = "data/"
 
-vis_raw_npy(RUNS,CH,POL,PATH) # Input variables should be lists of integers
+# OPT  = [AdjBaseLine,Norm,LogY]
+OPT  = [True,True,True]
+
+vis_raw_npy(RUNS,CH,POL,OPT,PATH) # Input variables should be lists of integers
