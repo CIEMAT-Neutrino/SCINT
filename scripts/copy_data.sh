3780583dedec0a01afaf0f97d19832b7bb30b5c3
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2022-10-25 17:54:33 +0200

    typo + data subfolders automatized

--- scripts/copy_data.sh
+++ scripts/copy_data.sh
@@ -7,9 +7,23 @@ if [ ! -d "data" ]; then
 mkdir data
 fi
 
+#raw data .root and .npy
+if [ ! -d "data/raw" ]; then
+mkdir data/raw
+fi
+#Analized data (charge, pedestal, peak...) variables
+if [ ! -d "data/ana" ]; then
+mkdir data/ana
+fi
+
+#Average wvfs 
+if [ ! -d "data/ave" ]; then
+mkdir data/ave
+fi
+
 #runes de interes: 2, 10, 22, 26
 
-scp pcae146.ciemat.es_outside:/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22_2/ROOT/\{run02_ch0.root,run02_ch1.root,run02_ch4.root,run02_ch6.root,run10_ch0.root,run10_ch1.root,run10_ch4.root,run10_ch6.root,run22_ch0.root,run22_ch1.root,run22_ch4.root,run22_ch6.root,run26_ch0.root,run26_ch1.root,run26_ch4.root,run26_ch6.root\} data/
+scp pcae146.ciemat.es_outside:/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22_2/ROOT/\{run02_ch0.root,run02_ch1.root,run02_ch4.root,run02_ch6.root,run10_ch0.root,run10_ch1.root,run10_ch4.root,run10_ch6.root,run22_ch0.root,run22_ch1.root,run22_ch4.root,run22_ch6.root,run26_ch0.root,run26_ch1.root,run26_ch4.root,run26_ch6.root\} data/raw
 
 # scp -r pcae146.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22_2/ROOT/run02_ch* data/
 # scp -r pcae146.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22_2/ROOT/run10_ch* data/
