dc0a2bd83f990d3e6ff3fc1258c2a46f8247029c
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2022-10-26 15:44:35 +0200

    added multiple wvf algorithms

--- lib/wvf_functions.py
+++ lib/wvf_functions.py
@@ -3,7 +3,7 @@ import numpy as np
 from .io_functions import load_analysis_npy, load_npy
 from itertools import product
 
-def average_wvfs(my_runs,PATH="../data/ave/"):
+def average_wvfs(my_runs,PATH="../data/ave/",threshold=50):
     
     try:
         ana_runs = load_analysis_npy(my_runs["N_runs"],my_runs["N_channels"])
@@ -12,8 +12,50 @@ def average_wvfs(my_runs,PATH="../data/ave/"):
         return 0
 
     for run,ch in product(my_runs["N_runs"],my_runs["N_channels"]):
-        # print(run,ch)
-        my_runs[run][ch]["ADC"] = ana_runs[run][ch]["P_channel"]*np.mean((my_runs[run][ch]["ADC"].T-ana_runs[run][ch]["Ped_mean"]).T,axis=0)
+        
+        # No centering
+        aux_ADC=ana_runs[run][ch]["P_channel"]*((my_runs[run][ch]["ADC"].T-ana_runs[run][ch]["Ped_mean"]).T);
+
+        my_runs[run][ch]["AvWvf"] = np.mean(aux_ADC,axis=0)
         aux_path=PATH+"Average_run"+str(run).zfill(2)+"_ch"+str(ch)+".npy"
+        
+        # centering
+        bin_ref         =np.argmax(aux_ADC[0]) #using the first peak as reference
+        # bin_ref         =int(len(aux_ADC[0])/15) #10% of time window
+        av_wvf_peak      =np.zeros(aux_ADC[0].shape)
+        av_wvf_threshold =np.zeros(aux_ADC[0].shape)
+
+        n_wvs   =len(aux_ADC)
+        n_bins  =len(aux_ADC[0])
+        for wvf in aux_ADC:
+            bin_peak      = np.argmax(wvf)
+            
+            try:
+                bin_threshold = np.argwhere(wvf>threshold)[0][0]
+            except:
+                #no good value found;
+                bin_threshold=n_bins
+
+            # Peak centering
+            if bin_ref<bin_peak:
+                av_wvf_peak[:bin_ref]                    += (wvf[(bin_peak-bin_ref):bin_peak]/n_wvs);
+                av_wvf_peak[bin_ref:-(bin_peak-bin_ref)] += (wvf[bin_peak:]/n_wvs);
+            else:
+                av_wvf_peak[(bin_ref-bin_peak):bin_ref] += (wvf[:bin_peak]/n_wvs);
+                av_wvf_peak[bin_ref:-1]                   += (wvf[bin_peak:-(bin_ref-bin_peak+1)]/n_wvs);
+            
+            # threshold centering
+            if bin_ref<bin_threshold:
+                av_wvf_threshold[:bin_ref]                    += (wvf[(bin_threshold-bin_ref):bin_threshold]/n_wvs);
+                av_wvf_threshold[bin_ref:-(bin_threshold-bin_ref)] += (wvf[bin_threshold:]/n_wvs);
+            else:
+                av_wvf_threshold[(bin_ref-bin_threshold):bin_ref] += (wvf[:bin_threshold]/n_wvs);
+                av_wvf_threshold[bin_ref:-1]                   += (wvf[bin_threshold:-(bin_ref-bin_threshold+1)]/n_wvs);
+        
+        
+        my_runs[run][ch]["AvWvf_peak"]=av_wvf_peak;
+        my_runs[run][ch]["AvWvf_threshold"]=av_wvf_threshold;
+        del my_runs[run][ch]["ADC"];
+
         np.save(aux_path,my_runs[run][ch])
         print("Saved data in:" , aux_path)
--- scripts/copy_data.sh
+++ scripts/copy_data.sh
@@ -23,7 +23,7 @@ fi
 
 #runes de interes: 2, 10, 22, 26
 
-scp pcae146.ciemat.es_outside:/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22_2/ROOT/\{run02_ch0.root,run02_ch1.root,run02_ch4.root,run02_ch6.root,run10_ch0.root,run10_ch1.root,run10_ch4.root,run10_ch6.root,run22_ch0.root,run22_ch1.root,run22_ch4.root,run22_ch6.root,run26_ch0.root,run26_ch1.root,run26_ch4.root,run26_ch6.root\} data/raw
+scp pcae146.ciemat.es_outside:/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22_2/ROOT/\{run02_ch0.root,run02_ch1.root,run02_ch4.root,run02_ch6.root,run10_ch0.root,run10_ch1.root,run10_ch4.root,run10_ch6.root,run22_ch0.root,run22_ch1.root,run22_ch4.root,run22_ch6.root,run26_ch0.root,run26_ch1.root,run26_ch4.root,run26_ch6.root,run28_ch0.root,run28_ch1.root,run28_ch4.root,run28_ch6.root,run29_ch0.root,run29_ch1.root,run29_ch4.root,run29_ch6.root\} data/raw
 
 # scp -r pcae146.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22_2/ROOT/run02_ch* data/
 # scp -r pcae146.ciemat.es:/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22_2/ROOT/run10_ch* data/
