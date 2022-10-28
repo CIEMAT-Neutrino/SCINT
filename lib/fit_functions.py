dbfa47a0b9dde60d8fb0d5c71cbb6522593a9f68
mantheys <sergio.manthey@gmail.com>
2022-10-28 11:02:43 +0200

    UPDATED_SC_FIT

--- lib/fit_functions.py
+++ lib/fit_functions.py
@@ -24,8 +24,8 @@ def func2sigma(T,P,A1,SIGMA1,SIGMA2,TAU1,T0,A2,TAU2):
 def logfunc2sigma(T,P,A1,SIGMA1,SIGMA2,TAU1,T0,A2,TAU2):
     return np.log(P + func(T,A1,SIGMA1,TAU1,T0) + func(T,A2,SIGMA2,TAU2,T0))
 
-def scfunc(T,Y):
-    return ([0]*np.exp(-(T-[2])/[1])/np.pow(2*np.pi,0.5)*np.exp(-[3]*[3]/([1]*[1])))*erf((([2]-T)/[3]+[3]/[1])/np.pow(2,0.5))+([4]*np.exp(-(T-[2])/[5])/np.pow(2*np.pi,0.5)*np.exp(-[3]*[3]/([5]*[5])))*erf((([2]-T)/[3]+[3]/[5])/np.pow(2,0.5))
+def scfunc(T,A,B,C,D,E,F):
+    return (A*np.exp(-(T-C)/B)/np.power(2*np.pi,0.5)*np.exp(-D**2/(B**2)))*(1-erf(((C-T)/D+D/B)/np.power(2,0.5)))+(E*np.exp(-(T-C)/F)/np.power(2*np.pi,0.5)*np.exp(-D**2/(F**2)))*(1-erf(((C-T)/D+D/F)/np.power(2,0.5)))
 
 def sipm_fit(my_runs,OPT):
     print("\n### WELCOME TO THE SiPM FIT ###")
@@ -62,19 +62,6 @@ def sipm_fit(my_runs,OPT):
             tau3 = 9e-6; tau3_low = tau2_high; tau3_high = 1e-5
             sigma2 = popt1[1]*10; sigma2_low = popt1[1]; sigma2_high = popt1[1]*100
 
-            # CHECK FIRST FIT
-            plt.ion()
-            next_plot = False
-            plt.rcParams['figure.figsize'] = [16, 8]
-            plt.subplot(1,2,1)
-            plt.title("First fit to determine peak")
-            plt.plot(RAW_X,RAW,label="RAW")
-            plt.plot(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],func(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],*popt1),label="FIT")
-            # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
-            plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
-            if check_key(OPT,"LOGY") != False: plt.semilogy();plt.ylim(thrld,RAW[MAX]*1.1)
-            plt.legend()
-
             # USING VALUES FROM FIRST FIT PERFORM SECONG FIT FOR THE SLOW COMPONENT
             bounds2 = ([sigma2_low,a2_low,tau2_low,a3_low,tau3_low],[sigma2_high,a2_high,tau2_high,a3_high,tau3_high])
             initial2 = (sigma2,a2,tau2,a3,tau3)
@@ -99,21 +86,37 @@ def sipm_fit(my_runs,OPT):
             for i in range(len(initial2)):
                 print("%s: \t%.2E \u00B1 %.2E"%(labels2[i],popt[i],perr2[i]))
             print("--------------------------------\n")
+            
+            try:
+                if OPT["SHOW"] == False: continue
+                else: pass
+            except:
+                print("SHOW key not included in OPT")
+                # CHECK FIRST FIT
+                plt.ion()
+                next_plot = False
+                plt.rcParams['figure.figsize'] = [16, 8]
+                plt.subplot(1,2,1)
+                plt.title("First fit to determine peak")
+                plt.plot(RAW_X,RAW,label="RAW")
+                plt.plot(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],func(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],*popt1),label="FIT")
+                # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
+                plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
+                if check_key(OPT,"LOGY") != False: plt.semilogy();plt.ylim(thrld,RAW[MAX]*1.1)
+                plt.legend()
 
-            plt.subplot(1,2,2)
-            plt.title("Second fit with full wvf")
-            plt.plot(RAW_X,RAW,zorder=0,c="tab:blue",label="RAW")
-            plt.plot(RAW_X[MAX-BUFFER:],func3(RAW_X[MAX-BUFFER:],*param),c="tab:orange",label="FIT")
-            plt.plot(RAW_X,func3(RAW_X,*param),c="tab:green",label="FIT_FULL_LENGHT")
-            plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
-            # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
-            if check_key(OPT,"LOGY") != False: plt.semilogy();plt.ylim(thrld,RAW[MAX]*1.1)
-            plt.legend()
-            while not plt.waitforbuttonpress(-1): pass
-
-            plt.clf()
-
-        plt.ioff()
+                plt.subplot(1,2,2)
+                plt.title("Second fit with full wvf")
+                plt.plot(RAW_X,RAW,zorder=0,c="tab:blue",label="RAW")
+                plt.plot(RAW_X[MAX-BUFFER:],func3(RAW_X[MAX-BUFFER:],*param),c="tab:orange",label="FIT")
+                plt.plot(RAW_X,func3(RAW_X,*param),c="tab:green",label="FIT_FULL_LENGHT")
+                plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
+                # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
+                if check_key(OPT,"LOGY") != False: plt.semilogy();plt.ylim(thrld,RAW[MAX]*1.1)
+                plt.legend()
+                while not plt.waitforbuttonpress(-1): pass
+                plt.clf()
+            plt.ioff()
 
 def scint_fit(my_runs,OPT):
     print("\n### WELCOME TO THE SCINTILLATION FIT ###")
@@ -144,19 +147,6 @@ def scint_fit(my_runs,OPT):
 
             OPT["CUT_NEGATIVE"] = True
             popt1,perr1 = peak_fit(RAW, RAW_X,BUFFER,OPT)
-
-            # CHECK FIRST FIT
-            plt.ion()
-            next_plot = False
-            plt.rcParams['figure.figsize'] = [16, 8]
-            plt.subplot(1,2,1)
-            plt.title("First fit to determine peak")
-            plt.plot(RAW_X,RAW,label="RAW")
-            plt.plot(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],func(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],*popt1),label="FIT")
-            # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
-            plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
-            if check_key(OPT,"LOGY") != False: plt.semilogy();plt.ylim(thrld,RAW[MAX]*1.1)
-            plt.legend()
 
             # USING VALUES FROM FIRST FIT PERFORM SECONG FIT FOR THE SLOW COMPONENT
             p = np.mean(RAW[:MAX-BUFFER])
@@ -190,20 +180,35 @@ def scint_fit(my_runs,OPT):
                 print("%s: \t%.2E \u00B1 %.2E"%(labels2[i],popt[i],perr2[i]))
             print("--------------------------------\n")
 
-            plt.subplot(1,2,2)
-            plt.title("Second fit with full wvf")
-            plt.plot(RAW_X,RAW,zorder=0,c="tab:blue",label="RAW")
-            plt.plot(RAW_X[MAX-BUFFER:],func2sigma(RAW_X[MAX-BUFFER:],*param),c="tab:orange",label="FIT")
-            plt.plot(RAW_X,func2sigma(RAW_X,*param),c="tab:green",label="FIT_FULL_LENGHT")
-            plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
-            # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
-            if check_key(OPT,"LOGY") != False: plt.semilogy();plt.ylim(thrld,RAW[MAX]*1.1)
-            plt.legend()
-            while not plt.waitforbuttonpress(-1): pass
-
-            plt.clf()
-
-        plt.ioff()
+            try:
+                if OPT["SHOW"] == False: continue
+                else: pass
+            except:
+                print("SHOW key not included in OPT")
+                # CHECK FIRST FIT
+                plt.ion()
+                next_plot = False
+                plt.rcParams['figure.figsize'] = [16, 8]
+                plt.subplot(1,2,1)
+                plt.title("First fit to determine peak")
+                plt.plot(RAW_X,RAW,label="RAW")
+                plt.plot(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],func(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],*popt1),label="FIT")
+                # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
+                plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
+                if check_key(OPT,"LOGY") != False: plt.semilogy();plt.ylim(thrld,RAW[MAX]*1.1)
+                plt.legend()
+                plt.subplot(1,2,2)
+                plt.title("Second fit with full wvf")
+                plt.plot(RAW_X,RAW,zorder=0,c="tab:blue",label="RAW")
+                plt.plot(RAW_X[MAX-BUFFER:],func2sigma(RAW_X[MAX-BUFFER:],*param),c="tab:orange",label="FIT")
+                plt.plot(RAW_X,func2sigma(RAW_X,*param),c="tab:green",label="FIT_FULL_LENGHT")
+                plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
+                # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
+                if check_key(OPT,"LOGY") != False: plt.semilogy();plt.ylim(thrld,RAW[MAX]*1.1)
+                plt.legend()
+                while not plt.waitforbuttonpress(-1): pass
+                plt.clf()
+            plt.ioff()
 
 def sc_fit(my_runs,OPT):
     print("\n### WELCOME TO THE SC FIT ###")        
@@ -221,68 +226,44 @@ def sc_fit(my_runs,OPT):
             LOOP = len(my_runs[run][ch][AVE])
         
         for i in range(LOOP):
-            
             if AVE == "ADC": RAW = ana_runs[run][ch]["P_channel"]*(my_runs[run][ch][AVE][i]-ana_runs[run][ch]["Ped_mean"][i])
             else: RAW = my_runs[run][ch][AVE]
-            
-            RAW_X = 4e-9*np.arange(len(RAW))
+
+            FIT_RAW_X = np.arange(len(RAW))
+            RAW_X = 4e-9*FIT_RAW_X
             MAX = np.argmax(RAW)
-            thrld = 1e-1
-            BUFFER = 100
-            buffer2 = 2000
 
-            popt1,perr1 = peak_fit(RAW,RAW_X,BUFFER,OPT)
+            try:
+                if OPT["SHOW"] == False: continue
+                else: pass
+            except:
+                print("'SHOW' key not included in OPT")
 
-            # CHECK FIRST FIT
+            # USING VALUES FROM FIRST FIT PERFORM SECONG FIT FOR THE SLOW COMPONENT
+            t0 = np.argmax(RAW)
+            initial = (500,1000,t0,20,-500,1000)
+            labels = ["AMP","TAU1","T0","SIGMA","AMP2","TAU2"]
+
+            popt, pcov = curve_fit(scfunc,FIT_RAW_X,RAW,p0 = initial, method = "trf")
+            perr = np.sqrt(np.diag(pcov))
+
+            print("\n--- FIT VALUES (SLOW) ---")
+            for i in range(len(initial)):
+                print("%s: \t%.2E \u00B1 %.2E"%(labels[i],popt[i],perr[i]))
+            print("--------------------------------\n")
+
             plt.ion()
             next_plot = False
-            plt.rcParams['figure.figsize'] = [16, 8]
-            plt.subplot(1,2,1)
-            plt.title("First fit to determine peak")
-            plt.plot(RAW_X,RAW,label="RAW")
-            plt.plot(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],func(RAW_X[MAX-BUFFER:MAX+int(BUFFER/2)],*popt1),label="FIT")
-            # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
+            plt.rcParams['figure.figsize'] = [8, 8]
+            plt.title("Fit with full wvf")
+            plt.plot(RAW_X,RAW,zorder=0,c="tab:blue",label="RAW")
+            plt.plot(RAW_X,scfunc(FIT_RAW_X,*popt),"tab:orange",label="FIT")
             plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
             if check_key(OPT,"LOGY") == True and OPT["LOGY"] == True: 
                 plt.semilogy()
                 plt.ylim(thrld,RAW[MAX]*1.1)
-            plt.legend()
-
-            # USING VALUES FROM FIRST FIT PERFORM SECONG FIT FOR THE SLOW COMPONENT
-            # p = np.mean(RAW[:MAX-BUFFER])
-            # a1 = 2e-5; a1_low = 1e-8;  a1_high = 9e-2                    
-            # a2 = 2e-5; a2_low = 1e-8; a2_high = 9e-2                    
-            # a3 = 2e-5; a3_low = 1e-8; a3_high = 9e-2
-            # tau1 = 9e-8; tau1_low = 6e-9; tau1_high = 1e-7
-            # tau2 = 9e-7; tau2_low = tau1_high; tau2_high = 1e-6
-            # tau3 = 9e-6; tau3_low = tau2_high; tau3_high = 1e-5
-            # sigma2 = popt1[1]*10; sigma2_low = popt1[1]; sigma2_high = popt1[1]*100
-            # bounds2 = ([sigma2_low,a3_low,tau3_low],[sigma2_high,a3_high,tau3_high])
-            # initial2 = (sigma2,a3,tau3)
-            # labels2 = ["SIG2","AMP2","TAU2"]
-
-            # popt, pcov = curve_fit(lambda T,SIGMA2,A3,TAU3: logfunc2sigma(T,p,a1,popt1[1],SIGMA2,tau1,popt1[3],A3,TAU3),RAW_X[MAX-BUFFER:buffer2],np.log(RAW[MAX-BUFFER:buffer2]),p0 = initial2, bounds = bounds2, method = "trf")
-            # perr2 = np.sqrt(np.diag(pcov))
-            # sigma2 = popt[0];a3 = popt[1];tau3 = popt[2]
-            # param = [p,a1,popt1[1],sigma2,tau1,popt1[3],a3,tau3]
-
-            # print("\n--- SECOND FIT VALUES (SLOW) ---")
-            # for i in range(len(initial2)):
-            #     print("%s: \t%.2E \u00B1 %.2E"%(labels2[i],popt[i],perr2[i]))
-            # print("--------------------------------\n")
-
-            # plt.subplot(1,2,2)
-            # plt.title("Second fit with full wvf")
-            # plt.plot(RAW_X,RAW,zorder=0,c="tab:blue",label="RAW")
-            # plt.plot(RAW_X[MAX-BUFFER:],func2sigma(RAW_X[MAX-BUFFER:],*param),c="tab:orange",label="FIT")
-            # plt.plot(RAW_X,func2sigma(RAW_X,*param),c="tab:green",label="FIT_FULL_LENGHT")
-            # plt.xlabel("Time in [s]"); plt.ylabel("ADC Counts")
-            # # plt.axvline(RAW_X[-buffer2],ls = "--",c = "k")
-            # if check_key(OPT,"LOGY") == True and OPT["LOGY"] == True: 
-            #     plt.semilogy()
-            #     plt.ylim(thrld,RAW[MAX]*1.1)
             
-            # plt.legend()
+            plt.legend()
             while not plt.waitforbuttonpress(-1): pass
 
             plt.clf()
