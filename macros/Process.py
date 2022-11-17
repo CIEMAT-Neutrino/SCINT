f36098ee42c3e163a0ffc9baa064a6e7ecac709f
mantheys <sergio.manthey@gmail.com>
2022-11-17 11:35:57 +0100

    CHANGE IN POLARITY INSERTION

--- macros/Process.py
+++ macros/Process.py
@@ -8,11 +8,11 @@ from itertools import product
 
 # Arrays used in load_run
 
-# RUNS=[48,49,50,62,63,64]
+RUNS=[9,10,11,25,26,27]
 RUNS_CALIB=[1,2,3]
 
-# CH=[0,1,4,5]
-CH_CALIB=[0,1,2,3,4]
+CH=[0,1,4,6]
+CH_CALIB=[0,1,6]
 
 # Missing variables to be inserted
 POLARITY       = [-1,-1,-1,-1]   #polarity
@@ -20,35 +20,34 @@ SAMPLING       = [4e-9,4e-9,4e-9,4e-9]
 LABELS         = ["SiPM","SiPM","PMT","SC"]
 LABELS_CALIB   = ["SiPM","SiPM","SC"]
 
-# # OTHER RUNS
-# # PRE-PROCESS RAW
-# for run, ch in product(RUNS,CH):
-#     # Start load_run 
-#     my_runs = load_npy([run], [ch])
+# OTHER RUNS
+# PRE-PROCESS RAW
+for run, ch in product(RUNS,CH):
+    # Start load_run 
+    my_runs = load_npy([run], [ch])
     
-#     insert_variable(my_runs,POLARITY,"P_channel")
-#     insert_variable(my_runs,SAMPLING,"Sampling")
-#     insert_variable(my_runs,LABELS,"Label")
+    insert_variable(my_runs,POLARITY,"P_channel")
+    insert_variable(my_runs,SAMPLING,"Sampling")
+    insert_variable(my_runs,LABELS,"Label")
     
-#     compute_peak_variables(my_runs)
-#     compute_pedestal_variables(my_runs)
-#     print(my_runs.keys())
+    compute_peak_variables(my_runs)
+    compute_pedestal_variables(my_runs)
+    print(my_runs.keys())
     
-#     save_proccesed_variables(my_runs,"","../data/raw/")
+    save_proccesed_variables(my_runs,"","../data/raw/")
 
-# # SECOND PROCESS - PROCESSED WAVEFORMS
-# for run, ch in product(RUNS,CH):
-#     my_runs = load_npy([run], [ch])
+# SECOND PROCESS - PROCESSED WAVEFORMS
+for run, ch in product(RUNS,CH):
+    my_runs = load_npy([run], [ch])
     
-#     compute_ana_wvfs(my_runs) 
-#     # Run appropiate ana_functions
-#     insert_variable(my_runs,[1,1,1,1],"P_channel") # Change polarity!
+    compute_ana_wvfs(my_runs) 
+    # Run appropiate ana_functions    
+    compute_peak_variables(my_runs,"Ana_ADC")
+    insert_variable(my_runs,[1,1,1,1],"P_channel") # Change polarity!
+    compute_pedestal_variables(my_runs,"Ana_ADC")
     
-#     compute_peak_variables(my_runs,"Ana_ADC")
-#     compute_pedestal_variables(my_runs,"Ana_ADC")
-    
-#     delete_keys(my_runs,["ADC"])
-#     save_proccesed_variables(my_runs,"Analysis_","../data/ana/")
+    delete_keys(my_runs,["ADC"])
+    save_proccesed_variables(my_runs,"Analysis_","../data/ana/")
 
 # CALIBRATION RUNS
 # PRE-PROCESS RAW
