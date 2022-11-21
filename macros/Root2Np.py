2fd8fd4d631cd89a6dbec62a1903867e9803e370
Laura Pérez-Molina <laurapermol@gmail.com>
2022-11-21 14:04:47 +0100

    update

--- macros/Root2Np.py
+++ macros/Root2Np.py
@@ -3,9 +3,9 @@ sys.path.insert(0, '../')
 from lib.io_functions import root2npy
 
 # RUNS=[1,2,3,9,10,11,25,26,27]
-RUNS=[1,12]
+RUNS    = [1,12]
 
 # CH=[0,1,4,6]
-CH=[0]
+CH      = [0]
 
 root2npy(RUNS,CH)
