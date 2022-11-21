20e7f5ce367e7fd054c6530806e8635befbf71d1
Laura Pérez-Molina <laurapermol@gmail.com>
2022-11-21 14:48:26 +0100

    1 bucle para calibration+otras runs

--- macros/Root2Np.py
+++ macros/Root2Np.py
@@ -2,8 +2,9 @@ import sys
 sys.path.insert(0, '../')
 from lib.io_functions import root2npy
 
-RUNS=[1,2,3,9,10,11,25,26,27]
-
-CH=[0,1,4,6]
+# Arrays used in load_run
+RUNS    = [1,5,12]
+# CH=[0,1,4,6] # SiPM1, SiPM2, PMT, XA
+CH      = [0]
 
 root2npy(RUNS,CH)
