7cb79c8ebfdd0830b718ca047711a7ea56fb8f7b
andtorre <andres.torre@ciemat.es>
2022-11-28 15:18:09 +0100

    NAMES_CHAMES

--- macros/Process.py
+++ macros/Process.py
@@ -10,30 +10,30 @@ from itertools import product
 input_file = input("Please select input File: ")
 info = read_input_file(input_file)
 
-RUNS = []; CH = []
-RUNS = np.append(RUNS,info["CALIB_RUNS"])
-RUNS = np.append(RUNS,info["LIGHT_RUNS"])
-RUNS = np.append(RUNS,info["ALPHA_RUNS"])
+runs = []; channels = []
+runs = np.append(runs,info["CALIB_RUNS"])
+runs = np.append(runs,info["LIGHT_RUNS"])
+runs = np.append(runs,info["ALPHA_RUNS"])
 
-CH = np.append(CH,info["CHAN_STNRD"])
+channels = np.append(channels,info["CHAN_STNRD"])
 
 """ To-Do: Avoid using loop in this macro. Maybe "ADC" type dict can be loaded in lazy mode """
 
 # PROCESS WAVEFORMS (Run in loop to avoid memory issues)
-for run, ch in product(RUNS.astype(int),CH.astype(int)):
+for run, ch in product(runs.astype(int),channels.astype(int)):
     
     my_runs = load_npy([run],[ch])
     compute_ana_wvfs(my_runs,debug=False)
 
-    delete_keys(my_runs,['Peak_amp', 'Peak_time', 'Ped_STD', 'Ped_mean', 'Ped_max', 'Ped_min', 'Ped_lim','P_channel']) # Delete raw peak and pedestal variables
-    insert_variable(my_runs,[1,1,1,1],"P_channel") # Change polarity!
+    delete_keys(my_runs,['PeakAmp', 'PeakTime', 'PedSTD', 'PedMean', 'PedMax', 'PedMin', 'PedLim','PChannel']) # Delete raw peak and pedestal variables
+    insert_variable(my_runs,[1,1,1,1],"PChannel") # Change polarity!
     
-    compute_peak_variables(my_runs,key="Ana_ADC")
-    compute_pedestal_variables(my_runs,key="Ana_ADC",debug=True)
+    compute_peak_variables(my_runs,key="AnaADC")
+    compute_pedestal_variables(my_runs,key="AnaADC",debug=True)
     
     delete_keys(my_runs,["ADC"])
 
     average_wvfs(my_runs)
-    integrate_wvfs(my_runs,["Ave_Limits"],"AvWvf",["DAQ", 250],[0,100])
+    integrate_wvfs(my_runs,["ChargeAveRange"],"AveWvf",["DAQ", 250],[0,100])
     
     save_proccesed_variables(my_runs,"Analysis_","../data/ana/")
