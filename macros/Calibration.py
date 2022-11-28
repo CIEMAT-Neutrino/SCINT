643a00c14d126eb04978791c3c20fe77a163ca2b
andtorre <andres.torre@ciemat.es>
2022-11-28 15:18:09 +0100

    NAMES_CHAMES

--- macros/Calibration.py
+++ macros/Calibration.py
@@ -5,33 +5,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 from itertools import product
 
-from lib.io_functions import load_npy,save_proccesed_variables
+from lib.io_functions import load_npy,save_proccesed_variables, read_input_file
 from lib.wvf_functions import average_SPE_wvfs
 from lib.cal_functions import calibrate
 
-N_runs_calib     = [1,2,3]
-N_channels_calib = [0,1,6]
+input_file = input("Please select input File: ")
+info = read_input_file(input_file)
 
-for run, ch in product(N_runs_calib,N_channels_calib):
+runs = []; channels = []
+runs = np.append(runs,info["CALIB_RUNS"])
+channels = np.append(channels,info["CHAN_STNRD"])      
+
+for run, ch in product(runs.astype(int),channels.astype(int)):
 # RUNS = load_npy(N_runs_calib,N_channels_calib,"Analysis_","../data/ana/") #Funciona dando los arrays pero sigue un orden distinto
-    RUNS = load_npy([run],[ch],"Analysis_","../data/ana/")
+    my_runs = load_npy([run],[ch],"Analysis_","../data/ana/")
 # AVE_RUNS = load_npy(N_runs_calib,N_channels_calib,"Average_","../data/ave/")
-    AVE_RUNS = load_npy([run],[ch],"Average_","../data/ave/")
-    INT_KEY = ["AVE_INT_LIMITS"]
+    ave_runs = load_npy([run],[ch],"Average_","../data/ave/")
+    int_key = ["ChargeAveRange"]
 
     OPT = {
         "LOGY": True,
         "PRINT_KEYS":False
           }
 
-    calibrate(RUNS,INT_KEY,OPT)
+    calibrate(my_runs,int_key,OPT)
 
     # print(RUNS[2][1]["Sampling"])
     # print(RUNS[2][1]["Sampling"])
 
-    average_SPE_wvfs(RUNS,AVE_RUNS,INT_KEY)
-    save_proccesed_variables(RUNS,"Analysis_","../data/ana/")
-    save_proccesed_variables(AVE_RUNS,"Average_","../data/ave/")
+    average_SPE_wvfs(my_runs,ave_runs,int_key)
+    save_proccesed_variables(my_runs,"Analysis_","../data/ana/")
+    save_proccesed_variables(ave_runs,"Average_","../data/ave/")
 
     # plt.plot(4e-9*np.arange(len(AVE_RUNS[2][6]["SPE_AvWvf"])),AVE_RUNS[2][6]["SPE_AvWvf"])
     # plt.show()
