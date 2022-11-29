09649bdc3dfdb00e985645ff75168a039c9254f1
andtorre <andres.torre@ciemat.es>
2022-11-29 17:10:30 +0100

    CUT_UPDATE

--- macros/Calibration.py
+++ macros/Calibration.py
@@ -17,25 +17,17 @@ runs = np.append(runs,info["CALIB_RUNS"])
 channels = np.append(channels,info["CHAN_STNRD"])      
 
 for run, ch in product(runs.astype(int),channels.astype(int)):
-# RUNS = load_npy(N_runs_calib,N_channels_calib,"Analysis_","../data/ana/") #Funciona dando los arrays pero sigue un orden distinto
     my_runs = load_npy([run],[ch],"Analysis_","../data/ana/")
-# AVE_RUNS = load_npy(N_runs_calib,N_channels_calib,"Average_","../data/ave/")
     ave_runs = load_npy([run],[ch],"Average_","../data/ave/")
     int_key = ["ChargeAveRange"]
 
     OPT = {
         "LOGY": True,
         "PRINT_KEYS":False
-          }
+        }
 
     calibrate(my_runs,int_key,OPT)
-
-    # print(RUNS[2][1]["Sampling"])
-    # print(RUNS[2][1]["Sampling"])
 
     average_SPE_wvfs(my_runs,ave_runs,int_key)
     save_proccesed_variables(my_runs,"Analysis_","../data/ana/")
     save_proccesed_variables(ave_runs,"Average_","../data/ave/")
-
-    # plt.plot(4e-9*np.arange(len(AVE_RUNS[2][6]["SPE_AvWvf"])),AVE_RUNS[2][6]["SPE_AvWvf"])
-    # plt.show()
