45ed16164c7c2be2cf22d105e7b89e3ca0426c47
andtorre <andres.torre@ciemat.es>
2022-12-01 15:56:39 +0100

    VIS_UPDATE

--- lib/cut_functions.py
+++ lib/cut_functions.py
@@ -1,7 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from itertools import product
 from .io_functions import check_key,print_keys,copy_single_run
+
+import matplotlib.pyplot as plt
+
+from itertools import product
+from .io_functions import check_key,print_keys,copy_single_run
+from .vis_functions import vis_var_hist
+from .fit_functions import gaussian,loggaussian,gaussian_train,loggaussian_train
 
 def generate_cut_array(my_runs):
     """
@@ -13,22 +21,30 @@ def generate_cut_array(my_runs):
                 ADC_key = key
         my_runs[run][ch]["MyCuts"] = np.ones(len(my_runs[run][ch][ADC_key]),dtype=bool)
 
-def cut_min_max(my_runs, keys, limits):
+def cut_min_max(my_runs, keys, limits, ranges = [0,0]):
     """
     This a fuction for cuts of min - max values. It takes a variable(s) and checks whether its value is between the specified limits.
     VARIABLES:
         - keys: a LIST of variables you want to constrain
         - limits: a DICTIONARY with same keys than variable "keys" and a list of the min and max values you want.
+        - ranges: a LIST with the range where we want to check the key value. If [0,0] it uses the whole window. Time in sec.
     Important! Each key works independently. If one key gives True and the other False, it remains False.
-    Example: keys = ["PeakAmp"], limits = {"PeakAmp": [20,50]}
+    Example: keys = ["PeakAmp", "PeakTime"], limits = {"PeakAmp": [20,50], "PeakTime": [4e-6, 5e-6]}
     """
     for run, ch, key in product(my_runs["NRun"], my_runs["NChannel"], keys):
         if check_key(my_runs[run][ch], "MyCuts") == True:
             if check_key(my_runs[run][ch], key) == True:
-                for i in range(len(my_runs[run][ch][key])):
-                    if limits[key][0] <= my_runs[run][ch][key][i] <= limits[key][1]:
-                       continue
-                    else: my_runs[run][ch]["MyCuts"][i] = False
+                if ranges[0]==0 and ranges[1]==0:
+                    for i in range(len(my_runs[run][ch][key])):
+                        if limits[key][0] <= my_runs[run][ch][key][i] <= limits[key][1]:
+                            continue    
+                        else: my_runs[run][ch]["MyCuts"][i] = False
+                else:
+                    i_idx = int(np.round(ranges[0]/my_runs[run][ch]["Sampling"])); f_idx = int(np.round(ranges[1]/my_runs[run][ch]["Sampling"]))
+                    for i in range(i_idx,f_idx+1):
+                        if limits[key][0] <= my_runs[run][ch][key][i] <= limits[key][1]:
+                            continue    
+                        else: my_runs[run][ch]["MyCuts"][i] = False
             else: print(key," does not exist in my_runs!")
         else: print("Run generate_cut_array")
 
@@ -53,19 +69,15 @@ def cut_min_max_sim(my_runs, keys, limits):
                     else: print(keys," does not exist in my_runs!")
         else: print("Run generate_cut_array")
 
-def cut_std(my_runs, keys, limits):
-    for run, ch, key in product(my_runs["NRun"], my_runs["NChannel"], keys):
-        data = my_runs[run][ch][key]
-        ypbot = np.percentile(data, 0.1); yptop = np.percentile(data, 0.99)
-        ypad = 0.2*(yptop - ypbot)
-        ymin = ypbot - ypad; ymax = yptop + ypad
-        data = [i for i in data if ymin<i<ymax]
-        for run, ch, key in product(my_runs["NRun"], my_runs["NChannel"], keys):
-            if check_key(my_runs[run][ch], "MyCuts") == True:
-                if check_key(my_runs[run][ch], key) == True:
-                    for i in range(len(my_runs[run][ch][key])):
-                        if limits[key][0] <= my_runs[run][ch][key][i] <= limits[key][1]:
-                        continue
-                        else: my_runs[run][ch]["MyCuts"][i] = False
-                else: print(key," does not exist in my_runs!")
-            else: print("Run generate_cut_array")
+# def cut_std(my_runs, keys, limits):
+#     for run, ch, key in product(my_runs["NRun"], my_runs["NChannel"], keys):
+#         data = my_runs[run][ch][key]
+#         ypbot = np.percentile(data, 0.1); yptop = np.percentile(data, 0.99)
+#         ypad = 0.2*(yptop - ypbot)
+#         ymin = ypbot - ypad; ymax = yptop + ypad
+#         data = [i for i in data if ymin<i<ymax]
+
+        
+
+
+
--- macros/Vis.py
+++ macros/Vis.py
@@ -5,21 +5,23 @@ sys.path.insert(0, '../')
 from lib.io_functions import *
 from lib.vis_functions import vis_npy, vis_var_hist
 
-runs     = [2]
-channels = [0,1]
+runs     = [99]
+channels = [0]
 
 OPT  = {
     "NORM":       False,           # Runs can be displayed normalised
     "LOGY":       False,           # Runs can be displayed in logy
-    "SHOW_AVE":   "AveWvf",        # If computed, vis will show average
+    "SHOW_AVE":   "AveWvfThreshold",        # If computed, vis will show average
     "SHOW_PARAM": True,            # Print terminal information
     "CHARGE_KEY": "ChargeAveRange" # Select charge info to be displayed. Default: "ChargeAveRange" (if computed)
     }
 
 my_runs = load_npy(runs,channels,"Analysis_","../data/ana/") # Load processed wvfs
+# my_runs = load_npy(runs,channels,"","../data/raw/") # Load processed wvfs
 keys = ["AnaADC"] # Choose between "ADC" or "AnaADC depending on wich run has been loaded"
+# keys = ["ADC"] # Choose between "ADC" or "AnaADC depending on wich run has been loaded"
 
-vis_npy(my_runs,keys,OPT) # Input variables should be lists of integers
+# vis_npy(my_runs,keys,OPT) # Input variables should be lists of integers
 
-# vis_var_hist(my_runs,["ChargeAveRange"],1e-3)
+vis_var_hist(my_runs,["PeakAmp"],[0.1,99.9])
 #"PeakAmp","PeakTime","PedSTD","ChargeAveRange"
