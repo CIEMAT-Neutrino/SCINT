04834f474a4ea4c8a8f03badbecc62a80117493c
andtorre <andres.torre@ciemat.es>
2023-01-10 16:27:04 +0100

    MEGA_POLY_CUT

--- lib/cut_functions.py
+++ lib/cut_functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
 from itertools import product
@@ -10,6 +12,9 @@ from itertools import product
 from .io_functions import check_key,print_keys,copy_single_run
 from .vis_functions import *
 from .fit_functions import gaussian,loggaussian,gaussian_train,loggaussian_train
+
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 def generate_cut_array(my_runs):
     """
@@ -24,7 +29,7 @@ def generate_cut_array(my_runs):
 
 def cut_min_max(my_runs, keys, limits, ranges = [0,0]):
     """
-    This a fuction for cuts of min - max values. It takes a variable(s) and checks whether its value is between the specified limits.
+    This is a fuction for cuts of min - max values. It takes a variable(s) and checks whether its value is between the specified limits.
     VARIABLES:
         - keys: a LIST of variables you want to constrain
         - limits: a DICTIONARY with same keys than variable "keys" and a list of the min and max values you want.
@@ -51,7 +56,7 @@ def cut_min_max(my_runs, keys, limits, ranges = [0,0]):
 
 def cut_min_max_sim(my_runs, keys, limits):
     """
-    This a fuction for cuts of min - max values. It takes a variable(s) and checks whether its value is between the specified limits.
+    This is a fuction for cuts of min - max values. It takes a variable(s) and checks whether its value is between the specified limits.
     VARIABLES:
         - keys: a LIST of variables you want to constrain at the same time
         - limits: a DICTIONARY with same keys than variable "keys" and a list of the min and max values you want.
@@ -96,15 +101,52 @@ def cut_min_max_sim(my_runs, keys, limits):
 #             else: print("Run generate_cut_array")
 
 def cut_lin_rel(my_runs, keys):
+    """
+    This is a function to cut manually with a polygonal figure on two variables. You can do any polygonal figure (avoid strange figures with crossed lines).
+    With "left click" you choose vertexes, with "right click" you delete the last vertex and with "middle click" you finish the figure.
+    VARIABLES:
+        - keys: a LIST of variables you want to plot and cut
+    """
     for run, ch in product(my_runs["NRun"], my_runs["NChannel"]):
         if check_key(my_runs[run][ch], "MyCuts") == True:
                 for j in range(len(keys)):
                     if check_key(my_runs[run][ch], keys[j]) == True:
                         continue
                     else: print("IAAA"); break
-                plt.scatter(my_runs[run][ch][keys[0]], my_runs[run] [ch][keys[1]])
-                # if limits[keys[j]][0] <= my_runs[run][ch][keys[j]][i] <= limits[keys[j]][1]:
-                #     my_runs[run][ch]["MyCuts"][i] = True
-                #     break
-                # else: my_runs[run][ch]["MyCuts"][i] = False
+                figure_features()
+                
+                matplotlib.rc('figure', figsize=(9, 6))
+                plt.scatter(my_runs[run][ch][keys[0]], my_runs[run] [ch][keys[1]], s = 15)
+                plt.grid("both")
+                plt.xlabel(keys[0]); plt.ylabel(keys[1])
+                coords = plt.ginput(100)
+                polygon = Polygon(coords)
+                n_points = len(coords)
+                print(n_points)
+                print("Nº evt totales", len(my_runs[run][ch]["MyCuts"]))
+                x_coords = []; y_coords = []; 
+                for k in range(n_points): x_coords.append(coords[k][0])
+                for k in range(n_points): y_coords.append(coords[k][1])
+                m_values = []; n_values = []
+                for i in range(n_points):
+                    if i == n_points-1:
+                        delta_y = y_coords[0] - y_coords[i]; delta_x = x_coords[0] - x_coords[i]
+                        m_values.append(delta_y / delta_x)
+                        n_values.append(y_coords[i] - m_values[i] * x_coords[i])
+                        x_aux = np.linspace(x_coords[i], x_coords[0], 1000); y_aux = x_aux*m_values[i] + n_values[i]
+                        plt.plot(x_aux, y_aux, "k--", alpha = 0.6)
+                    else:
+                        delta_y = y_coords[i+1] - y_coords[i]; delta_x = x_coords[i+1] - x_coords[i]
+                        m_values.append(delta_y / delta_x)
+                        n_values.append(y_coords[i] - m_values[i] * x_coords[i])
+                        x_aux = np.linspace(x_coords[i], x_coords[i+1], 1000); y_aux = x_aux*m_values[i] + n_values[i]
+                        plt.plot(x_aux, y_aux, "k--", alpha = 0.6)
+                for i in range(len(my_runs[run][ch][keys[0]])):
+                    point = Point(my_runs[run][ch][keys[0]][i], my_runs[run][ch][keys[1]][i])
+                    if  polygon.contains(point): my_runs[run][ch]["MyCuts"][i] = True
+                    else: my_runs[run][ch]["MyCuts"][i] = False
+
+                plt.scatter(my_runs[run][ch][keys[0]][my_runs[run][ch]["MyCuts"] == False],my_runs[run][ch][keys[1]][my_runs[run][ch]["MyCuts"] == False], c = "red", s = 15)
+                print("Nº cortados ", len(my_runs[run][ch]["MyCuts"][my_runs[run][ch]["MyCuts"] == False]))
+                plt.show()
         else: print("Run generate_cut_array")
