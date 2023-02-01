985cd4512283b16294be926e0f1d7d9bac7938d7
mantheys <sergio.manthey@gmail.com>
2023-02-01 15:01:13 +0100

    UPDATED PEAK AND PED BRANCH NAMES

--- lib/ana_functions.py
+++ lib/ana_functions.py
@@ -21,30 +21,29 @@ def insert_variable(my_runs, var, key, debug = False):
             KeyError
             if debug: print("Inserting value...")
 
-def compute_peak_variables(my_runs, range1 = 0, range2 = 0, key = "ADC", debug = False):
+def compute_peak_variables(my_runs, key = "ADC", label = "", debug = False):
     """Computes the peaktime and amplitude of a collection of a run's collection in standard format"""
     # to do: implement ranges 
     for run,ch in product(my_runs["NRun"],my_runs["NChannel"]):
         try:
-            my_runs[run][ch]["PeakAmp" ] = np.max    (my_runs[run][ch][key][:,:]*my_runs[run][ch]["PChannel"],axis=1)
-            my_runs[run][ch]["PeakTime"] = np.argmax (my_runs[run][ch][key][:,:]*my_runs[run][ch]["PChannel"],axis=1)
+            my_runs[run][ch][label+"PeakAmp" ] = np.max    (my_runs[run][ch][key][:,:]*my_runs[run][ch][label+"PChannel"],axis=1)
+            my_runs[run][ch][label+"PeakTime"] = np.argmax (my_runs[run][ch][key][:,:]*my_runs[run][ch][label+"PChannel"],axis=1)
             print("Peak variables have been computed for run %i ch %i"%(run,ch))
         except: 
             KeyError
             if debug: print("*EXCEPTION: for ",run,ch,key," peak variables could not be computed")
 
-def compute_pedestal_variables(my_runs, key = "ADC", debug = False):
+def compute_pedestal_variables(my_runs, key = "ADC", label = "", buffer = 200, debug = False):
     """Computes the pedestal variables of a collection of a run's collection in standard format"""
     for run,ch in product(np.array(my_runs["NRun"]).astype(int),np.array(my_runs["NChannel"]).astype(int)):
         try:
-            buffer = 200
             ped_lim = st.mode(my_runs[run][ch]["PeakTime"])[0][0]-buffer
             if ped_lim < 0: ped_lim = 50
-            my_runs[run][ch]["PedSTD"]  = np.std (my_runs[run][ch][key][:,:ped_lim],axis=1)
-            my_runs[run][ch]["PedMean"] = np.mean(my_runs[run][ch][key][:,:ped_lim],axis=1)
-            my_runs[run][ch]["PedMax"]  = np.max (my_runs[run][ch][key][:,:ped_lim],axis=1)
-            my_runs[run][ch]["PedMin"]  = np.min (my_runs[run][ch][key][:,:ped_lim],axis=1)
-            my_runs[run][ch]["PedLim"]  = ped_lim
+            my_runs[run][ch][label+"PedSTD"]  = np.std (my_runs[run][ch][key][:,:ped_lim],axis=1)
+            my_runs[run][ch][label+"PedMean"] = np.mean(my_runs[run][ch][key][:,:ped_lim],axis=1)
+            my_runs[run][ch][label+"PedMax"]  = np.max (my_runs[run][ch][key][:,:ped_lim],axis=1)
+            my_runs[run][ch][label+"PedMin"]  = np.min (my_runs[run][ch][key][:,:ped_lim],axis=1)
+            my_runs[run][ch][label+"PedLim"]  = ped_lim
             print("Pedestal variables have been computed for run %i ch %i"%(run,ch))
         except: 
             KeyError
@@ -54,14 +53,14 @@ def compute_ana_wvfs(my_runs, debug = False):
     """Computes the peaktime and amplitude of a collection of a run's collection in standard format"""
     for run,ch in product(np.array(my_runs["NRun"]).astype(int),np.array(my_runs["NChannel"]).astype(int)):
         try:
-            my_runs[run][ch]["AnaADC"] = my_runs[run][ch]["PChannel"]*((my_runs[run][ch]["ADC"].T-my_runs[run][ch]["PedMean"]).T)
+            my_runs[run][ch]["ADC"] = my_runs[run][ch]["RawPChannel"]*((my_runs[run][ch]["RawADC"].T-my_runs[run][ch]["RawPedMean"]).T)
             print("Analysis wvfs have been computed for run %i ch %i"%(run,ch))
             if debug: print_keys(my_runs)
         except: 
             KeyError
             if debug: print("*EXCEPTION: for ",run,ch," ana wvfs could not be computed")
         if debug:
-            plt.plot(4e-9*np.arange(len(my_runs[run][ch]["AnaADC"][0])),my_runs[run][ch]["AnaADC"][0])
+            plt.plot(4e-9*np.arange(len(my_runs[run][ch]["ADC"][0])),my_runs[run][ch]["ADC"][0])
             plt.show()
 
 def get_units(my_runs, debug = False):
@@ -78,5 +77,3 @@ def get_units(my_runs, debug = False):
             else:
                 aux_dic[key] = "a.u."
         my_runs[run][ch]["Units"] = aux_dic
-    
-
