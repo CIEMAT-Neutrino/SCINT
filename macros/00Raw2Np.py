8d7be3177b3f4bab5ab5e9ea43f2e41ef902b5e8
mantheys <sergio.manthey@gmail.com>
2023-02-01 15:01:54 +0100

    MINOR CHANGES

--- macros/00Raw2Np.py
+++ macros/00Raw2Np.py
@@ -1,5 +1,4 @@
-# ---------------------------------------------------------------------------------------------------------------------- #
-#  ¡!¡!¡! RUN ONLY IF DATA HAS NOT YET BEEN CONVERTED TO NPY ¡!¡!¡!                                                      #
+# ---------------------------------------------------------------------------------------------------------------------- #                                                     #
 #  ============================================ RUN:$ python3 00Raw2Np.py TEST ========================================= #
 # This macro will convert RAW DATA into NPY (NPZ that are npy compressed) files                                          #
 # Ideally we want to work in /pnfs/ciemat.es/data/neutrinos/FOLDER and so we mount the folder in our computer with:      #
@@ -26,17 +25,19 @@ info = read_input_file(input_file)
 
 runs = []; channels = []
 runs = np.append(runs,info["CALIB_RUNS"])
-# runs = np.append(runs,info["LIGHT_RUNS"])
-# runs = np.append(runs,info["ALPHA_RUNS"])
+runs = np.append(runs,info["LIGHT_RUNS"])
+runs = np.append(runs,info["ALPHA_RUNS"])
 # runs = np.append(runs,info["MUONS_RUNS"])
 
 channels = np.append(channels,info["CHAN_STNRD"])
 
 # From the input txt file you can choose the extension of your input file
-if info["RAW_DATA"][0] == "ROOT":
-    print("----- Taking a .root file as input data -----")
-    root2npy(runs.astype(int),channels.astype(int),info=info)
+
+# NEEDS UPDATE!
+# if info["RAW_DATA"][0] == "ROOT":
+#     print("----- Taking a .root file as input data -----")
+#     root2npy(runs.astype(int),channels.astype(int),info=info)
 
 if info["RAW_DATA"][0] == "DAT":
     print("----- Taking a .dat file as input data -----")
-    binary2npy(runs.astype(int),channels.astype(int),info=info,compressed=True, header_lines=6)
+    binary2npy(runs.astype(int),channels.astype(int),info=info,compressed=True,force=True)
