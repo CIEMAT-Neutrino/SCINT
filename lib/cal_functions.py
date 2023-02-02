66b216edbe97292e33aa66c2cb72a764761b8913
Laura Pérez-Molina <laurapermol@gmail.com>
2023-02-02 15:45:41 +0100

    CALIBRATION COMPLETE

--- lib/cal_functions.py
+++ lib/cal_functions.py
@@ -1,15 +1,13 @@
+import os
 import numpy as np
-# import copy
-# import sys
-
 import scipy
 from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
-
-import matplotlib.pyplot as plt
-
 from itertools import product
-from .io_functions import check_key,print_keys
+import matplotlib.pyplot as plt
+# import copy
+
+from .io_functions  import check_key,print_keys
 from .vis_functions import vis_var_hist
 from .fit_functions import gaussian,gaussian_train, loggaussian, loggaussian_train
 from .ana_functions import generate_cut_array, get_units
@@ -24,8 +22,8 @@ def calibrate(my_runs, keys, OPT={}):
     plt.ion()
     next_plot = False
     idx = 0
-    save_calibration=[]
     for run, ch, key in product(my_runs["NRun"], my_runs["NChannel"], keys):        
+        
         if check_key(my_runs[run][ch], "MyCuts") == False:
             generate_cut_array(my_runs)
         if check_key(my_runs[run][ch], "Units") == False:
@@ -42,6 +40,8 @@ def calibrate(my_runs, keys, OPT={}):
 
             counts, bins, bars = vis_var_hist(my_runs, run, ch, key, OPT=OPT)
             plt.close()
+
+            # New Figure with the fit
             fig_cal, ax_cal = plt.subplots(1,1, figsize = (8,6))
 
             add_grid(ax_cal)
@@ -68,15 +68,14 @@ def calibrate(my_runs, keys, OPT={}):
             ax_cal.plot(x[peak_idx], y[peak_idx], 'r.', lw=4)
             ax_cal.plot(x[valley_idx], y[valley_idx], 'b.', lw=6)
 
-            height,center,width = [],[],[]
-            initial = []
+            initial = [] #Saving for input to the TRAIN FIT
             
             n_peaks = 6
             if len(peak_idx)-1 < n_peaks:
-                n_peaks = len(peak_idx)-1
+                n_peaks = len(peak_idx)-1 #Number of peaks found by find_peak
             
             for i in range(n_peaks):
-                x_space = np.linspace(x[peak_idx[i]],x[peak_idx[i+1]],acc)
+                x_space = np.linspace(x[peak_idx[i]], x[peak_idx[i+1]], acc) #Array with values between the x_coord of 2 consecutives peaks
                 step    = x_space[1]-x_space[0]
                 x_gauss = x_space-int(acc/2)*step
                 x_gauss = x_gauss[x_gauss >= bins[0]]
@@ -86,12 +85,11 @@ def calibrate(my_runs, keys, OPT={}):
                 try:
                     # popt, pcov = curve_fit(loggaussian,x_gauss,np.log10(y_gauss),p0=[np.log10(y[peak_idx[i]]),x[peak_idx[i]],abs(wdth*(bins[0]-bins[1]))])
                     popt, pcov = curve_fit(gaussian,x_gauss,y_gauss,p0=[y[peak_idx[i]],x[peak_idx[i]],abs(wdth*(bins[0]-bins[1]))])
-                    # height.append(popt[0])
-                    # center.append(popt[1])
-                    # width.append(popt[2])
-                    initial.append(popt[1])
-                    initial.append(popt[0])
-                    initial.append(popt[2])
+                    perr = np.sqrt(np.diag(pcov))
+                    # FITTED to gaussian(x, height, center, width)
+                    initial.append(popt[1])         # HEIGHT
+                    initial.append(popt[0])         # CENTER
+                    initial.append(np.abs(popt[2])) # WIDTH
                     ax_cal.plot(x_gauss,gaussian(x_gauss, *popt), ls = "--", c = "black", alpha = 0.5)
                 
                 except:
@@ -101,13 +99,16 @@ def calibrate(my_runs, keys, OPT={}):
                     print("Peak %i could not be fitted"%i)
 
             try:
+                ## GAUSSIAN TRAIN FIT ## Taking as input parameters the individual gaussian fits with initial
                 # popt, pcov = curve_fit(loggaussian_train,x[:peak_idx[-1]],np.log10(y[:peak_idx[-1]]),p0=initial)
-                popt, pcov = curve_fit(gaussian_train,x[:peak_idx[-1]], y[:peak_idx[-1]], p0=initial)
+                popt, pcov = curve_fit(gaussian_train,x[:peak_idx[-1]], y[:peak_idx[-1]], p0=initial) 
+                perr = np.sqrt(np.diag(pcov))
             except:
                 popt = initial
                 print("Full fit could not be performed")
             ax_cal.plot(x[:peak_idx[-1]],gaussian_train(x[:peak_idx[-1]], *popt), label="")
             # plt.legend()
+
             if check_key(OPT,"LOGY") == True and OPT["LOGY"] == True:
                 ax_cal.semilogy()
             
@@ -126,7 +127,8 @@ def calibrate(my_runs, keys, OPT={}):
             # print("SPE max charge for run %i ch %i = %.2E"%(run,ch,popt[3] + abs(popt[5])))
             my_runs[run][ch]["MinChargeSPE"] = popt[3] - abs(popt[5])
             # print("SPE min charge for run %i ch %i = %.2E"%(run,ch,popt[3] - abs(popt[5])))
-            save_calibration.append(popt)
+
+            
         except KeyError:
             print("Empty dictionary. No calibration to show.")
     
@@ -134,4 +136,84 @@ def calibrate(my_runs, keys, OPT={}):
     plt.clf()
     plt.close()
     
-    return save_calibration
+    return popt, pcov, perr
+
+
+def compute_cal_parameters(popt, pcov):
+    """Computes calibration parameters for each peak.
+       \n Given popt and pcov which are the output for the best parameters when performing the Gaussian fit.
+       \n It returns an array of arrays: 
+       save_calibration = [ [[mu,dmu],[height,dheight],[height,dheight],[sigma,dsigma],
+                            [gain,dgain],[sn0,dsn0],[sn1,dsn1],[sn2,dsn2]], [PEAK 1], [PEAK 2],...]
+    """
+    
+    cal_parameters = []
+    perr = np.sqrt(np.diag(pcov))    #error for each variable
+    fitted_peaks = int(len(popt)/3)  #three parameters fitted for each peak
+    for i in np.arange(fitted_peaks): 
+        # print("\nPeak:", i)
+        mu     = [popt[(i+0)+2*i], perr[(i+0)+2*i]]  # mu +- dmu
+        height = [popt[(i+1)+2*i], perr[(i+1)+2*i]] # height +- dheight (not saving in txt by default)
+        sigma  = [popt[(i+2)+2*i], perr[(i+2)+2*i]]  # sigma +- dsigma
+        cal_parameters.append([mu,height,sigma])
+        copy_cal = cal_parameters
+        # print("MU +- DMU:",         save_calibration[i][0])
+        # print("HEIGHT +- DHEIGHT:", save_calibration[i][1])
+        # print("SIGMA +- DSIGMA:",   save_calibration[i][2])
+
+    for i in np.arange(fitted_peaks): #distances between peaks
+        if i == fitted_peaks-1:
+            gain = -99; dgain = -99; sn0 = -99; dsn0 = -99; sn1 = -99; dsn1 = -99; sn2 = -99; dsn2 = -99
+        else:
+            # GAIN = [mu(i+1) - mu(i)] * 1e-12 /1.602e-19 (pC)
+            gain  = (copy_cal[i+1][0][0]-copy_cal[i][0][0])*1e-12/1.602e-19; dgain = (np.sqrt(copy_cal[i+1][0][1]**2+copy_cal[i][0][1]**2))*1e-12/1.602e-19
+            
+            # SN0 = [mu(i+1)-mu(i)]/sigma(i)
+            sn0 = (copy_cal[i+1][0][0]-copy_cal[i][0][0])/copy_cal[i][2][0]
+            dsn0 = sn0 * np.sqrt(((copy_cal[i+1][0][1]**2+copy_cal[i][0][1]**2)/((copy_cal[i+1][0][0]-copy_cal[i][0][0])))**2+(copy_cal[i][2][1]/copy_cal[i][2][0])**2)
+            
+            # SN1 = [mu(i+1)-mu(i)]/sigma(i+1)
+            sn1 = (copy_cal[i+1][0][0]-copy_cal[i][0][0])/copy_cal[i+1][2][0]
+            dsn1 = sn1 * np.sqrt(((copy_cal[i+1][0][1]**2+copy_cal[i][0][1]**2)/((copy_cal[i+1][0][0]-copy_cal[i][0][0]))**2)+(copy_cal[i+1][2][1]/copy_cal[i+1][2][0])**2)
+            
+            # SNC = [mu(i+1)-mu(i)]/sqrt(sigma(i)**2+sigma(i+1)**2)
+            sn2 = (copy_cal[i+1][0][0]-copy_cal[i][0][0])/(np.sqrt(copy_cal[i][2][0]**2+copy_cal[i+1][2][0]**2))
+            dsn2 = sn2 * np.sqrt((dgain/gain)**2+((copy_cal[i][2][0]*copy_cal[i][2][1])/((copy_cal[i][2][0])**2+(copy_cal[i+1][2][0])**2))**2+((copy_cal[i+1][2][0]*copy_cal[i+1][2][1])/((copy_cal[i][2][0])**2+(copy_cal[i+1][2][0])**2))**2)
+
+        cal_parameters[i].append([gain, dgain]); 
+        cal_parameters[i].append([sn0, dsn0]);cal_parameters[i].append([sn1, dsn1]);cal_parameters[i].append([sn2, dsn2])
+    return cal_parameters
+
+
+def calibration_txt(run, ch, info, array_parameters):
+    """Save in a txt the calibration parameters to be exported directly.
+       Takes as input an array of arrays with the computed parameters (see compute_cal_parameters())
+    """
+
+    fitted_peaks = len(array_parameters)
+    for i in np.arange(fitted_peaks): #three parameters fitted for each peak
+                print("\nPeak:", i)
+                print("MU +- DMU:",         array_parameters[i][0])
+                print("HEIGHT +- DHEIGHT:", array_parameters[i][1])
+                print("SIGMA +- DSIGMA:",   array_parameters[i][2])
+                print("GAIN +- DGAIN:",     array_parameters[i][3])
+                print("SN0 +- DSN0:",       array_parameters[i][4])
+                print("SN1 +- DSN1:",       array_parameters[i][5])
+                print("SN2 +- DSN2:",       array_parameters[i][6])
+    
+    confirmation = input("Confirmation to save in ../fit_data/gain_ch%i.txt the printed parameters (except HEIGHT) (y/n) ?"%ch)
+    if "y" in confirmation:
+        header_list = ["RUN","OV","PEAK","MU","DMU","SIG","DSIG","GAIN","DGAIN","SN0","DSN0","SN1","DSN1","SN2","DSN2"]
+        if not os.path.exists("../fit_data/gain_ch%i.txt"%ch): #HEADER#
+            with open("../fit_data/gain_ch%i.txt"%ch, 'a+') as f:
+                f.write("\t".join(header_list)+"\n")
+
+        with open("../fit_data/gain_ch%i.txt"%ch, 'a+') as f:
+            for i in np.arange(fitted_peaks): #three parameters fitted for each peak
+            
+                f.write(str(int(run))+"\t"+info["OV_LABEL"][0]+"\t"+str(i)+"\t") #OVLABEL no funciona bien
+                for k in [0,2,3,4,5]:
+                    if k == 3:
+                        f.write("\t")
+                    f.write(str(array_parameters[i][k][0]) +"\t" + str(array_parameters[i][k][1])+"\t")
+                f.write(str(array_parameters[i][-1][0]) +"\t" + str(array_parameters[i][6][1])+"\n")
