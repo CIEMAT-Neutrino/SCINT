cc3aebf9c8c6298a21f8a83e911d6b07720c4b34
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-02-02 10:49:20 +0100

    added documentatiion to first data process library

--- lib_new/first_data_process.py
+++ lib_new/first_data_process.py
@@ -1,10 +1,10 @@
-import numpy as np
 from itertools import product
+import numpy as np
 import pandas as pd
 
 
-def binary2npy(FileName,header_lines=6):
-    """Dumps root file with given header lines(6) and wvf size defined in header. \n
+def Bin2Npz_ADC(FileName,header_lines=6):
+    """Dumps ADC binary .dat file with given header lines(6) and wvf size defined in header. \n
     Returns a npy array with Raw Wvf 
     If binary files are modified(header/data types), ask your local engineer"""
     
@@ -22,22 +22,22 @@ def binary2npy(FileName,header_lines=6):
         print("Header:",header)
         print("Waveform Samples:",NSamples)
         print("Event_size(wvf+header):",Event_size)
-        print("N_Events:",N_Events)
         
+        print("N_Events:",N_Events)
     #reshape everything, delete unused header
     data=np.reshape(data,(N_Events,Event_size))[:,header_lines*2:]
 
     return data;
 
 
-def bin2npy_compressed(file_in,file_out):
+def save_Bin2Npz_compressed(file_in,file_out):
     """Self-explainatory. Computation time x10 slower than un-compresed, size x3 times smaller"""
-    data_npy=binary2npy(file_in)
+    data_npy=Bin2Npz_ADC(file_in)
     np.savez_compressed(file_out,data_npy)
 
 
-def DumpBin2Npz(Run,Channel,in_path="data/raw/",out_path="data/raw/") :
-    
+def save_Run_Bin2Npz(Run,Channel,in_path="data/raw/",out_path="data/raw/") :
+    """Run is an int, channel is an int array. In/out paths are strings."""
     for ch in Channel:
         inchan =in_path+"run"+str(Run).zfill(2)+"/wave"+str(ch)+".dat"
         outchan=out_path+"run"+str(Run).zfill(2)+"/RawADC"+str(ch)  
@@ -45,11 +45,11 @@ def DumpBin2Npz(Run,Channel,in_path="data/raw/",out_path="data/raw/") :
         print("-----------------")
         print("Dumping: ",inchan," to: ",outchan+".npz")
         print("-----------------")
-        bin2npy_compressed(inchan,outchan)
+        save_Bin2Npz_compressed(inchan,outchan)
 
-def Bin2Npz_excel(excel_file_path=""):
+def Bin2Npz_excel(excel_file_path="",sheet='Sheet1'):
+    """Calls the dumping function using a excel table with the data runs of our"""
+    df = pd.read_excel(excel_file_path, sheet_name=sheet)
+    df['Channel']=df['Channel'].apply(lambda x: list(map(int,x.split(",")))) #excell only allows one value per cell, convert channels from string to array of ints
 
-    df = pd.read_excel(excel_file_path, sheet_name='Sheet1')
-    df['Channel']=df['Channel'].apply(lambda x: list(map(int,x.split(",")))) #excell only allows one value per cell, convert string to array of channels
-
-    df.apply(lambda x: DumpBin2Npz(x["Run"],x["Channel"]),axis=1);
+    df.apply(lambda x: save_Run_Bin2Npz(x["Run"],x["Channel"]),axis=1);
