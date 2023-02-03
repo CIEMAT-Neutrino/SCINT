9faa532d5578148d5a2ad058605ff2375537a8b5
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-02-03 16:26:32 +0100

    non-compressed format added v2

---
+++ new_macros/Substract_pedestal.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.insert(0, '../')
+
+from lib_new.io_lib  import open_run_var,open_run_properties,do_run_things,save_run_var
+from lib_new.ped_lib import compute_Pedestal,substract_Pedestal
+from lib.header import print_header
+import gc #garbage collector interface
+
+
+# for run in [1,2,3,9,10,11,25,26,27]:
+for run in [1,2,3]:
+    path="../data/raw/run"+str(run).zfill(2)+"/";
+    Run_props=open_run_properties(run,"Runs_list.xlsx")
+
+    ADC=open_run_var(path,"RawADC",Run_props["Channels"])
+    
+    Pedestal=do_run_things(ADC,compute_Pedestal)
+    ADC=do_run_things((ADC,Pedestal,Run_props["Polarity"]),substract_Pedestal)
+    
+    del Run_props
+    gc.collect()#free memory
+    
+    # save_run_var(Pedestal,path,"Pedestal")
+    # save_run_var(ADC,path,"ADC")
+    
+    del Pedestal, ADC
+    gc.collect()#free memory
+
