210d0601abec87f8b0db5fe91ed8ec991de198ac
mantheys <sergio.manthey@gmail.com>
2023-02-14 21:20:46 +0100

    GENERATE NEW INPUT FILE AFTER DEC STAGE

--- macros/06Deconvolution.py
+++ macros/06Deconvolution.py
@@ -13,58 +13,60 @@ except IndexError:
     input_file = input("Please select input File: ")
 
 info = read_input_file(input_file)
-channels = []
-raw_runs = np.asarray(info["ALPHA_RUNS"]).astype(int)
-dec_runs = np.asarray(info["LIGHT_RUNS"]).astype(int)
-ref_runs = np.asarray(info["CALIB_RUNS"]).astype(int)
-SiPM_OV = 1 # Choose between OV value for SiPM in alpha run (0, 1 or 2)
-ana_ch = np.asarray(info["CHAN_TOTAL"]).astype(int)
+generate_input_file(info,label="Dec")
 
-# for run, ch in product(np.arange(0,len(raw_runs)),np.arange(0,len(ana_ch))):
-for idx, run in enumerate(raw_runs):
-    for jdx, ch in enumerate(ana_ch):
-        my_runs = load_npy([run],[ch],preset="ANA",info=info,compressed=True)  # Select runs to be deconvolved (tipichaly alpha)     
+# channels = []
+# raw_runs = np.asarray(info["ALPHA_RUNS"]).astype(int)
+# dec_runs = np.asarray(info["LIGHT_RUNS"]).astype(int)
+# ref_runs = np.asarray(info["CALIB_RUNS"]).astype(int)
+# SiPM_OV = 1 # Choose between OV value for SiPM in alpha run (0, 1 or 2)
+# ana_ch = np.asarray(info["CHAN_TOTAL"]).astype(int)
 
-        if "SiPM" in str(my_runs[run][ch]["Label"]):
-            light_runs =  load_npy([dec_runs[SiPM_OV]],[ch],preset="CUTS",info=info,compressed=True) # Select runs to serve as dec template (tipichaly light)    
-            single_runs = load_npy([ref_runs[SiPM_OV]],[ch],preset="CUTS",info=info,compressed=True) # Select runs to serve as dec template scaling (tipichaly SPE)   
-        elif "SC" in str(my_runs[run][ch]["Label"]):
-            light_runs =  load_npy([dec_runs[idx]],[ch],preset="CUTS",info=info,compressed=True) # Select runs to serve as dec template (tipichaly light)    
-            single_runs = load_npy([ref_runs[idx]],[ch],preset="CUTS",info=info,compressed=True) # Select runs to serve as dec template scaling (tipichaly SPE)
-        else:
-            print("UNKNOWN DETECTOR LABEL!")
+# # for run, ch in product(np.arange(0,len(raw_runs)),np.arange(0,len(ana_ch))):
+# for idx, run in enumerate(raw_runs):
+#     for jdx, ch in enumerate(ana_ch):
+#         my_runs = load_npy([run],[ch],preset="ANA",info=info,compressed=True)  # Select runs to be deconvolved (tipichaly alpha)     
+
+#         if "SiPM" in str(my_runs[run][ch]["Label"]):
+#             light_runs =  load_npy([dec_runs[SiPM_OV]],[ch],preset="EVA",info=info,compressed=True) # Select runs to serve as dec template (tipichaly light)    
+#             single_runs = load_npy([ref_runs[SiPM_OV]],[ch],preset="EVA",info=info,compressed=True) # Select runs to serve as dec template scaling (tipichaly SPE)   
+#         elif "SC" in str(my_runs[run][ch]["Label"]):
+#             light_runs =  load_npy([dec_runs[idx]],[ch],preset="EVA",info=info,compressed=True) # Select runs to serve as dec template (tipichaly light)    
+#             single_runs = load_npy([ref_runs[idx]],[ch],preset="EVA",info=info,compressed=True) # Select runs to serve as dec template scaling (tipichaly SPE)
+#         else:
+#             print("UNKNOWN DETECTOR LABEL!")
         
-        keys = ["AveWvf","SER","AveWvf"] # keys contains the 3 labels required for deconvolution keys[0] = raw, keys[1] = det_response and keys[2] = deconvolution 
+#         keys = ["AveWvf","SER","AveWvf"] # keys contains the 3 labels required for deconvolution keys[0] = raw, keys[1] = det_response and keys[2] = deconvolution 
 
-        generate_SER(my_runs, light_runs, single_runs)
+#         generate_SER(my_runs, light_runs, single_runs)
 
-        OPT = {
-            "NOISE_AMP": 1,
-            "FIX_EXP":True,
-            "LOGY":True,
-            "NORM":False,
-            "FOCUS":False,
-            "SHOW": True,
-            "SHOW_F_SIGNAL":True,
-            "SHOW_F_GSIGNAL":True,
-            "SHOW_F_DET_RESPONSE":True,
-            "SHOW_F_GAUSS":True,
-            "SHOW_F_WIENER":True,
-            "SHOW_F_DEC":True,
-            "WIENER_BUFFER": 800,
-            "THRLD": 1e-4,
-            }
+#         OPT = {
+#             "NOISE_AMP": 1,
+#             "FIX_EXP":True,
+#             "LOGY":True,
+#             "NORM":False,
+#             "FOCUS":False,
+#             "SHOW": True,
+#             "SHOW_F_SIGNAL":True,
+#             "SHOW_F_GSIGNAL":True,
+#             "SHOW_F_DET_RESPONSE":True,
+#             "SHOW_F_GAUSS":True,
+#             "SHOW_F_WIENER":True,
+#             "SHOW_F_DEC":True,
+#             "WIENER_BUFFER": 800,
+#             "THRLD": 1e-4,
+#             }
 
-        deconvolve(my_runs,keys=keys,OPT=OPT)
+#         deconvolve(my_runs,keys=keys,OPT=OPT)
 
-        OPT = {
-            "SHOW": False,
-            "FIXED_CUTOFF": True
-            }
+#         OPT = {
+#             "SHOW": False,
+#             "FIXED_CUTOFF": True
+#             }
 
-        keys[0] = "ADC"
-        keys[2] = "ADC"
-        deconvolve(my_runs,keys=keys,OPT=OPT)
+#         keys[0] = "ADC"
+#         keys[2] = "ADC"
+#         deconvolve(my_runs,keys=keys,OPT=OPT)
 
-        save_proccesed_variables(my_runs,preset="DEC",info=info,force=True)
-        del my_runs,light_runs,single_runs
+#         save_proccesed_variables(my_runs,preset="DEC",info=info,force=True)
+#         del my_runs,light_runs,single_runs
