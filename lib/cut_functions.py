d6e0dc31eee88bb9c8ad8b1f1a972bead227eeae
mantheys <sergio.manthey@gmail.com>
2023-02-16 14:42:50 +0100

    UPDATE BUG Qt5Agg

--- lib/cut_functions.py
+++ lib/cut_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib
-matplotlib.use('Qt5Agg')
+# matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 from itertools import product
 from shapely.geometry         import Point
