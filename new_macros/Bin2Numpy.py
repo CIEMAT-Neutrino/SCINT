fd5bbea31368a8ba022cb5d9fe51c1ffd81ee01c
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-02-24 14:24:54 +0100

    SBND first run list added, added input/output paths for first data processing

--- new_macros/Bin2Numpy.py
+++ new_macros/Bin2Numpy.py
@@ -4,4 +4,7 @@ sys.path.insert(0, '../')
 import pandas as pd
 from lib_new.first_data_process import Bin2Np_excel
 
-Bin2Np_excel("Runs_list.xlsx")
+in_path ="/media/rodrigoa/2Gb/data/SBND_XA_PDE/SBND_XA_VIS/raw/"
+out_path="/media/rodrigoa/2Gb/data/SBND_XA_PDE/SBND_XA_VIS/joython/"
+
+Bin2Np_excel("Runs_list.xlsx",compressed=False,i_path=in_path,o_path=out_path)
