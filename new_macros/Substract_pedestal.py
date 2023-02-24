aea10470386018e372215f86836567d3c855cc42
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-02-24 15:48:59 +0100

    updated substract pedestal

--- new_macros/Substract_pedestal.py
+++ new_macros/Substract_pedestal.py
@@ -7,10 +7,12 @@ from lib.header import print_header
 import gc #garbage collector interface
 
 
-# for run in [1,2,3,9,10,11,25,26,27]:
-for run in [2]:
-    path="../data/raw/run"+str(run).zfill(2)+"/";
+path ="/media/rodrigoa/2Gb/data/SBND_XA_PDE/SBND_XA_VIS/joython/"
+
+for run in range(30):
+    if run==3:continue # Rodrigo forgot one run :D
     Run_props=open_run_properties(run,"Runs_list.xlsx")
+    run_path=path+"run"+str(run).zfill(2)+"/";
 
     compress=False
     
@@ -20,13 +22,13 @@ for run in [2]:
     # remember to always delete between runs to prevent overloading)
 
     for ch in Run_props["Channels"]:
-        ADC=open_run_var(path,"RawADC",[ch],compressed=compress)
+        ADC=open_run_var(run_path,"RawADC",[ch],compressed=compress)
         
         Pedestal_vars=do_run_things(ADC,compute_Pedestal)
         ADC=do_run_things((ADC,Pedestal_vars,Run_props["Polarity"]),substract_Pedestal)
         
-        # save_run_var(ADC,path,"ADC",compressed=compress) #no real need to save this, ~1s to load and substract pedestals anyway;
-        save_run_var(Pedestal_vars,path,"Pedestal_vars")
+        # save_run_var(ADC,run_path,"ADC",compressed=compress) #no real need to save this, ~1s to load and substract pedestals anyway;
+        save_run_var(Pedestal_vars,run_path,"Pedestal_vars")
         
         del Pedestal_vars, ADC
 
