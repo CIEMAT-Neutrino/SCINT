f7f4f94a407ca7f085d470652c31bfb78e6197ba
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-02-24 14:24:54 +0100

    SBND first run list added, added input/output paths for first data processing

--- lib_new/first_data_process.py
+++ lib_new/first_data_process.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import gc #garbage collector interface
+import os
 
 def Bin2Np_ADC(FileName,header_lines=6):
     """Dumps ADC binary .dat file with given header lines(6) and wvf size defined in header. \n
@@ -40,6 +41,7 @@ def save_Bin2Np(file_in,file_out,compressed=True):
 
 def save_Run_Bin2Np(Run,Channel,in_path="../data/raw/",out_path="../data/raw/",out_name="RawADC",Compressed=True) :
     """Run is an int, channel is an int array. In/out paths are strings."""
+    os.system("mkdir -p " + out_path+"run"+str(Run).zfill(2)+"/") # create output folder if not present
     for ch in Channel:
         inchan =in_path+"run"+str(Run).zfill(2)+"/wave"+str(ch)+".dat"
         outchan=out_path+"run"+str(Run).zfill(2)+"/"+out_name+"_ch"+str(ch)  
@@ -52,6 +54,8 @@ def save_Run_Bin2Np(Run,Channel,in_path="../data/raw/",out_path="../data/raw/",o
 def Bin2Np_excel(excel_file_path="",sheet='Sheet1',compressed=True,i_path="",o_path=""):
     """Calls the dumping function using a excel table with the data runs of our"""
     df = pd.read_excel(excel_file_path, sheet_name=sheet,engine='openpyxl')
-    df['Channels']=df['Channels'].apply(lambda x: list(map(int,x.split(",")))) #excell only allows one value per cell, convert channels from string to array of ints
+    
+    # df['Channels'].apply(lambda x: print(x.split(" "))) #excell only allows one value per cell, convert channels from string to array of ints
+    df['Channels']=df['Channels'].apply(lambda x: list(map(int,x.split(" ")))) #excell only allows one value per cell, convert channels from string to array of ints
 
     df.apply(lambda x: save_Run_Bin2Np(x["Run"],x["Channels"],Compressed=compressed,in_path=i_path,out_path=o_path),axis=1,);
