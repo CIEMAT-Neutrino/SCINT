3b351472dd650bdce07e885ca2f09d1d19fc5883
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-02-24 15:28:26 +0100

    slightly modified av wvf version

--- lib_new/average_waveforms.py
+++ lib_new/average_waveforms.py
@@ -5,13 +5,13 @@
 
 import numpy as np
 
-def Average_waveform(ADCs,mode="simple",shift=[]):
+def Average_waveform(ADCs,mode="simple",shift=None):
     """Template function computing average waveform for a fixed channel
 
     Args:
-        ADCs (np.array): ADC input must be a 2dim array, where every row is a waveform
-        mode (str): simple=no shift, if alignement necessary use shift instead.
-        shift (np.array):
+        ADCs (numpy.ndarray): ADC input must be a 2D array, where every row is a waveform
+        mode (str): simple=no shift, if alignement necessary use shifted instead.
+        shift (numpy.ndarray): If present, Must be a numpy 1Darray, with entries equal to number of rows of input wvf 
     
     Returns:
         Average WVF: np array with number of entries equal to number of columns of input ADCs
@@ -19,11 +19,13 @@ def Average_waveform(ADCs,mode="simple",shift=[]):
     
     if mode=="simple":
         return np.mean(ADCs,axis=1) #Good ol mean
-    elif mode == "shift":# Shift the wvfs
+    
+    elif mode == "shifted":
         N_wvfs=len(ADCs)
         for i in range(N_wvfs):
-            ADCs[i]=shift4_numba(ADCs[i],shift[i])
-        return Average_waveform(ADCs) #once aligned, compute the mean
+            ADCs[i]=shift4_numba(ADCs[i],shift[i]) # Shift the wvfs
+        
+        return Average_waveform(ADCs) #once aligned, call simple mode
 
 
 
