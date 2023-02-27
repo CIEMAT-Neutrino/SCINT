85ba04b871a00b245083b9ece117798b04f3da42
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-02-27 10:56:53 +0100

    Andres se llevó el disco duro, numeración a las macros de procesación de la datación

--- new_macros/00_Bin2Numpy.py
+++ new_macros/00_Bin2Numpy.py
@@ -4,7 +4,8 @@ sys.path.insert(0, '../')
 import pandas as pd
 from lib_new.first_data_process import Bin2Np_excel
 
-in_path ="/media/rodrigoa/2Gb/data/SBND_XA_PDE/SBND_XA_VIS/raw/"
-out_path="/media/rodrigoa/2Gb/data/SBND_XA_PDE/SBND_XA_VIS/joython/"
+# in_path ="/media/rodrigoa/2Gb/data/SBND_XA_PDE/SBND_XA_VIS/raw/"
+in_path ="/media/rodrigoa/DiscoDuro/SBND_XA_VIS/"
+out_path="/media/rodrigoa/DiscoDuro/SBND_XA_VIS/joython/"
 
 Bin2Np_excel("Runs_list.xlsx",compressed=False,i_path=in_path,o_path=out_path)
