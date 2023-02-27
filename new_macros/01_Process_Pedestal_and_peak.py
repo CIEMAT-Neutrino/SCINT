e6c832431a8561c29a3afd09dade30fa985766fe
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-02-27 10:57:18 +0100

    Faltaba esto

--- new_macros/01_Process_Pedestal_and_peak.py
+++ new_macros/01_Process_Pedestal_and_peak.py
@@ -2,14 +2,15 @@ import sys
 sys.path.insert(0, '../')
 
 from lib_new.io_lib  import open_run_var,open_run_properties,do_run_things,save_run_var
-from lib_new.ped_lib import compute_Pedestal,substract_Pedestal
+from lib_new.ped_lib import compute_Pedestal,substract_Pedestal, compute_Peak
 from lib.header import print_header
 import gc #garbage collector interface
 
 
-path ="/media/rodrigoa/2Gb/data/SBND_XA_PDE/SBND_XA_VIS/joython/"
+# path ="/media/rodrigoa/2Gb/data/SBND_XA_PDE/SBND_XA_VIS/joython/"
+path="/media/rodrigoa/DiscoDuro/SBND_XA_VIS/joython/"
 
-for run in range(1):
+for run in range(30):
     if run==3:continue # Rodrigo forgot one run :D
     Run_props=open_run_properties(run,"Runs_list.xlsx")
     run_path=path+"run"+str(run).zfill(2)+"/";
@@ -27,8 +28,10 @@ for run in range(1):
         Pedestal_vars=do_run_things(ADC,compute_Pedestal)
         ADC=do_run_things((ADC,Pedestal_vars,Run_props["Polarity"]),substract_Pedestal)
         
+        Peak_vars=do_run_things(ADC,compute_Peak)
         # save_run_var(ADC,run_path,"ADC",compressed=compress) #no real need to save this, ~1s to load and substract pedestals anyway;
         save_run_var(Pedestal_vars,run_path,"Pedestal_vars",compressed=compress)
+        save_run_var(Peak_vars,run_path,"Peak_vars",compressed=compress)
         
         del Pedestal_vars, ADC
 
