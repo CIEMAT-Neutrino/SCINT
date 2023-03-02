6a9eb81092bfe7055bc28d46ea583e6eeca8bfe2
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-03-02 17:01:36 +0100

    cut interface added

--- lib_new/cuts.py
+++ lib_new/cuts.py
@@ -1,6 +1,7 @@
 """This Library provides basic interface for cuts. Dictionarys(channels and vars) are converted to pandasDataframes with Multiindexing [channel][variable]
 """
 import pandas as pd
+import copy 
 
 def VARsAsDataFrame(VARs:tuple) -> pd.DataFrame:
     """Transforms a tuple of input variables into a multiindexed pandas dataframe (easier to perform cuts with)
@@ -23,3 +24,19 @@ def VARsAsDataFrame(VARs:tuple) -> pd.DataFrame:
 
 def SWAP_COLUM_LEVELS(DF: pd.DataFrame):
     return DF.swaplevel(0,1,1).sort_index(1)
+
+
+def Apply_cut_toVAR(df:pd.DataFrame,VAR)->dict:
+    
+    aux_VAR=copy.deepcopy(VAR) #copy
+     
+    cut_as_dict=df.to_dict('series')
+    if type(VAR[  list(VAR.keys())[0]  ])==dict: #nested dictionaries, apply on subdictionary
+        for channel in VAR:
+            for var in VAR[channel]: 
+                aux_VAR[channel][var]=VAR[channel][var][cut_as_dict[channel]]
+        return aux_VAR;
+    else:#single dictionary, one key per channel
+        for channel in VAR:
+            aux_VAR[channel]=VAR[channel][cut_as_dict[channel]]
+        return aux_VAR;
