e64d750e275f7874dde2f6e41061d4111aaef707
Laura Perez-Molina <Laura.Perez@ciemat.es>
2023-03-03 16:30:21 +0100

    prueba

--- macros/0XVisTests.py
+++ macros/0XVisTests.py
@@ -24,8 +24,8 @@ channels = [int(c) for c in input_channels.split(",")]
 OPT  = {
     "MICRO_SEC":   True,
     "NORM":        False,                # Runs can be displayed normalised (True/False)
-    "LOGY":        False,               # Runs can be displayed in logy (True/False)
-    "SHOW_AVE":    "AveWvfSPE",             # If computed, vis will show average (AveWvf,AveWvfSPE,etc.)
+    "LOGY":        False,                # Runs can be displayed in logy (True/False)
+    "SHOW_AVE":    "AveWvf",             # If computed, vis will show average (AveWvf,AveWvfSPE,etc.)
     "SHOW_PARAM":  True,                 # Print terminal information (True/False)
     "CHARGE_KEY":  "ChargeAveRange",     # Select charge info to be displayed. Default: "ChargeAveRange" (if computed)
     "PEAK_FINDER": False,                # Finds possible peaks in the window (True/False)
@@ -35,26 +35,27 @@ OPT  = {
 
 ##### LOAD RUNS #####
 # my_runs = load_npy(runs,channels,preset="RAW",info=info,compressed=True)
-my_runs = load_npy(runs,channels,preset="ANA",info=info,compressed=True)
-# my_runs = load_npy(runs,channels,preset="CUTS",info=info,compressed=True) # Fast load (no ADC)
-print(my_runs[runs[0]][channels[0]].keys())
+# my_runs = load_npy(runs,channels,preset="ALL",info=info,compressed=True)
+my_runs = load_npy(runs,channels,branch_list=["ADC","AveWvf","PedSTD","Sampling","Label","PedMean","PedMin","PedMax","PedLim","PeakAmp","PeakTime"],info=info,compressed=True)
+# my_runs = load_npy(runs,channels,preset="EVA",info=info,compressed=True) # Fast load (no ADC)
+print_keys(my_runs)
 #####################
 
 ##### EVENT VISUALIZER #####
 # vis_npy(my_runs, ["RawADC"], evt_sel = -1, same_plot = False, OPT = OPT) # Input variables should be lists of integers
-# vis_npy(my_runs, ["ADC"], evt_sel = -1, same_plot = False, OPT = OPT) # Input variables should be lists of integers
 #####################
 
 ##### CUTS #####
 # cut_min_max(my_runs, ["PedMax"], {"PedMax": [-99,14]})
 # cut_min_max_sim(my_runs, ["PeakAmp", "ChargeAveRange"], {"PeakAmp": [30,45], "ChargeAveRange": [1.2, 2]})
 # cut_lin_rel(my_runs, ["PeakAmp", "ChargeAveRange"])
+vis_npy(my_runs, ["ADC"], evt_sel = -1, same_plot = False, OPT = OPT) # Input variables should be lists of integers
 ################
 
 ##### HISTOGRAMS #####
-for r in runs:
-    for c in channels:
-        vis_var_hist(my_runs, r, c, "RawPedMean", [0.1,99.9], OPT = {"SHOW": True})
+# for r in runs:
+#     for c in channels:
+#         vis_var_hist(my_runs, r, c, "RawPedMean", [0.1,99.9], OPT = {"SHOW": True})
 #         vis_var_hist(my_runs, r, c, "ChargeAveRange",[0.1,99.9], {"SHOW": True})
         # vis_two_var_hist(my_runs, r, c, ["PedMax", "PedAmp"], OPT = {"SHOW": True})
 ######################
