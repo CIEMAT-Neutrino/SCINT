daa773149a11eec98c8ef96ea1524b3f4b30a055
andtorre <andres.torre@ciemat.es>
2023-03-09 14:34:55 +0100

    A

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,12 @@
-import os, copy
-import math
+import os, copy, gc, math
+
 import matplotlib.pyplot as plt
 import numpy             as np
 import scipy             as sc
+import pandas            as pd
+import plotly.express    as px
+import plotly.io         as pio
+import gc
 from scipy.signal   import find_peaks
 from scipy.optimize import curve_fit
 from scipy.special  import erf
