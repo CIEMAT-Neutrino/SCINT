5b4071cff12ab63f5bc4a9bfa0938d2f92587030
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2023-04-04 10:25:55 +0200

    some updates

--- joython/lib/vis_functions.py
+++ joython/lib/vis_functions.py
@@ -9,7 +9,7 @@ def horizontal_subplots(x,ys,yer,legend):
     fig.subplots_adjust(wspace=0)
 
     for i in range(len(legend)):
-        axs[i].errorbar (x,ys[i],fmt='', yerr=yer[i],color= colors[i],linewidth=1)
+        axs[i].errorbar (x,ys[i], yerr=[yer[i],yer[i]],color= colors[i],linewidth=1,marker='o',markersize=3,elinewidth=1,capsize=2)
         axs[i].set_ylim([2,10])
         axs[i].legend([legend[i]])
         axs[i].grid()
