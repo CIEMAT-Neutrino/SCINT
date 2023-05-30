668205e3c256ef7b4c1491c4e028c2e352f48029
Laura Pérez-Molina <laurapermol@gmail.com>
2023-05-30 10:06:48 +0200

    deleted sipms ir02 analysis --> new repo

--- BORRAR/sipms_analysis/lib/__init__.py
+++
@@ -1,8 +0,0 @@
-
-import os
-import numpy             as np
-import matplotlib.pyplot as plt
-import pandas            as pd
-import xlrd, csv
-
-from .sipms_functions import *
--- BORRAR/sipms_analysis/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "../fit_data" ]; then
-mkdir ../fit_data
-fi
-
-pip install --upgrade pip
-pip3 install xlrd==1.2.0
