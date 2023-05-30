9103b2bd42f1df5123779809c48defee3cf578af
Laura Pérez-Molina <laurapermol@gmail.com>
2023-05-30 09:59:33 +0200

    tutorial

---
+++ 00TutorialMacros/03Integration.py
@@ -0,0 +1,47 @@
+# ---------------------------------------------------------------------------------------------------------------------- #
+#  ========================================= RUN:$ python3 03Integration.py TEST ======================================= #
+# This macro will calculate integration parametes of all individual wvfs, to be used in subsequent steps of calibration, #
+# charge and deconvolution.                                                                                              #
+# Ideally we want to work in /pnfs/ciemat.es/data/neutrinos/FOLDER and so we mount the folder in our computer with:      #
+# $ sshfs USER@pcaeXYZ.ciemat.es:/pnfs/ciemat.es/data/neutrinos/FOLDER ../data  --> making sure empty data folder exists #
+# ---------------------------------------------------------------------------------------------------------------------- #
+
+import sys; sys.path.insert(0, '../'); from lib import *; print_header()
+try:               input_file = sys.argv[1]
+except IndexError: input_file = input("Please select input File: ")
+
+info = read_input_file(input_file)
+runs = []; channels = []
+runs = np.append(runs,info["CALIB_RUNS"])
+runs = np.append(runs,info["LIGHT_RUNS"])
+runs = np.append(runs,info["ALPHA_RUNS"])
+runs = np.append(runs,info["MUONS_RUNS"])
+
+channels = np.append(channels,info["CHAN_TOTAL"])
+
+# my_runs = load_npy(runs.astype(int),channels.astype(int),preset="ANA",info=info,compressed=True)
+
+for run, ch in product(runs.astype(int),channels.astype(int)):
+    
+    my_runs = load_npy([run],[ch],preset=str(info["LOAD_PRESET"][3]),info=info,compressed=True)
+    # my_runs = load_npy([run],[ch],preset="EVA",info=info,compressed=True)
+    
+    #### CUT SECTION ####
+    # cut_min_max(my_runs, ["PedSTD"], {"PedSTD": [-1,7.5]})
+    # cut_lin_rel(my_runs, ["PeakAmp","ChargeAveRange"])
+    # cut_peak_finder(my_runs, ["ADC"], 2)
+    #####################
+
+    #### Align indivual waveforms + Average ####
+    # average_wvfs(my_runs,centering="NONE") # Compute average wvfs VERY COMPUTER INTENSIVE!
+    # average_wvfs(my_runs,centering="PEAK") # Compute average wvfs VERY COMPUTER INTENSIVE!
+    # average_wvfs(my_runs,centering="THRESHOLD") # Compute average wvfs EVEN MORE COMPUTER INTENSIVE!
+
+    ## Charge Integration ##
+    integrate_wvfs(my_runs, info = info) # Compute charge according to selected average wvf from input file ("AveWvf", "AveWvfPeak", "AveWvfThreshold")
+    # charge_nevents(my_runs)
+
+    # delete_keys(my_runs,["GaussADC"]) # Delete branches to avoid overwritting
+    save_proccesed_variables(my_runs,preset=str(info["SAVE_PRESET"][3]),info=info, force=True)
+    del my_runs
+    gc.collect()
---
+++ 00TutorialMacros/0XVisTests.py
@@ -0,0 +1,79 @@
+# ---------------------------------------------------------------------------------------------------------------------- #
+#  ========================================= RUN:$ python3 0XVisTests.py 0 0,1,2 ======================================= #
+# This macro will show the individual EVENTS of the introduced runs and channels to see if everything is working fine    #
+# Ideally we want to work in /pnfs/ciemat.es/data/neutrinos/FOLDER and so we mount the folder in our computer with:      #
+# $ sshfs USER@pcaeXYZ.ciemat.es:/pnfs/ciemat.es/data/neutrinos/FOLDER ../data  --> making sure empty data folder exists #
+# ---------------------------------------------------------------------------------------------------------------------- #
+
+import sys; sys.path.insert(0, '../'); from lib import *; # print_header()
+import plotly.offline as py
+
+##### INPUT RUNS AND OPTIONS #####
+try:
+    input_file     = sys.argv[1]
+    input_runs     = sys.argv[2]
+    input_channels = sys.argv[3]
+except IndexError:
+    input_file     = input("Please select input file (e.g Feb22_2): ")
+    input_runs     = input("Please select RUNS (separated with commas): ")
+    input_channels = input("Please select CHANNELS (separated with commas): ")
+
+info     = read_input_file(input_file)
+runs     = [int(r) for r in input_runs.split(",")]
+channels = [int(c) for c in input_channels.split(",")]
+
+OPT  = {
+    "MICRO_SEC":   True,
+    "NORM":        False,                # Runs can be displayed normalised (True/False)
+    "ALIGN":       False,
+    "LOGY":        False,               # Runs can be displayed in logy (True/False)
+    "SHOW_AVE":    "",             # If computed, vis will show average (AveWvf,AveWvfSPE,etc.)
+    "SHOW_PARAM":  False,                 # Print terminal information (True/False)
+    "CHARGE_KEY":  "ChargeAveRange",     # Select charge info to be displayed. Default: "ChargeAveRange" (if computed)
+    "PEAK_FINDER": False,                # Finds possible peaks in the window (True/False)
+    "LEGEND":      True,                # Shows plot legend (True/False)
+    "SHOW":        True,
+    "CHARGEDICT":  False
+    }
+###################################
+
+##### LOAD RUNS #####
+# my_runs = load_npy(runs,channels,preset="RAW",info=info,compressed=True) # Load to visualize raw events
+# my_runs = load_npy(runs,channels,preset="ANA",info=info,compressed=True) # Load to visualize processed events
+my_runs = load_npy(runs, channels,preset="EVA",info=info,compressed=True) # Fast load (no ADC)
+#####################
+##### READ CHARGE DICTIONARY #####
+if OPT["CHARGEDICT"] == True:
+    for r in runs:
+        for c in channels:
+            dicti = np.load(info["PATH"][0]+info["MONTH"][0]+"/npy/run"+str(r).zfill(2)+"_ch"+str(c)+"/ChargeRangeDict.npz",allow_pickle=True, mmap_mode="w+")["arr_0"].item()
+            print("ChargeRanges for RUN", r, " and CHANNEL ",c, " :\n", dicti, "\n")
+
+##### CUTS #####
+cut_ped_std(my_runs, n_std = 2, chs_cut=[0,1,3,4,5], apply_all_chs=True)
+# cut_min_max(my_runs, ["PeakAmp","PeakTime"], {"PeakAmp": [60,650],"PeakTime": [3.7e-6,4e-6]}, chs_cut=[0,1,3,4], apply_all_chs=True)
+# cut_min_max(my_runs, ["PeakAmp","PeakTime"], {"PeakAmp": [1400,4500],"PeakTime": [3.7e-6,4e-6]}, chs_cut=[5], apply_all_chs=True)
+# cut_min_max(my_runs, ["PeakAmp","PeakTime"], {"PeakAmp": [2500,7500],"PeakTime": [3.7e-6,4e-6]}, chs_cut=[5], apply_all_chs=True)
+# cut_min_max(my_runs, ["PeakAmp","PeakTime"], {"PeakAmp": [5000,13300],"PeakTime": [3.7e-6,4e-6]}, chs_cut=[5], apply_all_chs=True)
+# cut_lin_rel(my_runs, ["PeakAmp","ChargeAveRange"], compare = "NONE", percentile=[0,100])
+# cut_peak_finder(my_runs, ["ADC"], 2)
+
+##### VISUALIZE HISTOGRAMS #####
+vis_var_hist(my_runs, ["PeakTime"], compare = "NONE", percentile = [0,100],OPT = OPT, select_range=False)
+vis_var_hist(my_runs, ["ChargeAveRange","ChargeRange0"], compare = "CHANNELS", percentile = [0.1,99.9], OPT = OPT, select_range=False) 
+vis_two_var_hist(my_runs, ["PeakAmp", "ChargeAveRange"], compare = "CHANNELS", OPT = {"SHOW": True}, percentile=[0.1,99.9], select_range=False)
+
+##### EVENT VISUALIZER #####
+# vis_npy(my_runs, ["RawADC"], evt_sel = -1, same_plot = False, OPT = OPT, debug=True) # Input variables should be lists of integers
+# vis_npy(my_runs, ["ADC"], evt_sel = -1, same_plot = False, OPT = OPT) # Input variables should be lists of integers
+# vis_npy(my_runs, ["GaussADC"], evt_sel = -1, same_plot = False, OPT = OPT) # Input variables should be lists of integers
+
+##### AVERAGE WAVEFORMS VISUALIZER #####
+vis_compare_wvf(my_runs, ["AveWvf"], compare = "CHANNELS", OPT = OPT) # Input variables should be lists of integers
+
+######################
+#### AVERAGE WAVEFORM MAKER ####
+# average_wvfs(my_runs,centering="NONE",cut_label="Pruebasss") # Compute average wvfs VERY COMPUTER INTENSIVE!
+# average_wvfs(my_runs,centering="PEAK") # Compute average wvfs VERY COMPUTER INTENSIVE!
+# average_wvfs(my_runs,centering="THRESHOLD", threshold=60) # Compute average wvfs EVEN MORE COMPUTER INTENSIVE!
+# save_proccesed_variables(my_runs,preset="EVA",info=info, force=True)
