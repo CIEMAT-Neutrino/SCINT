2da52d2eb1d83b9a026e7e70b5c2a360a5bc6ca5
mantheys <sergio.manthey@gmail.com>
2023-08-10 19:10:07 +0200

    CLEANUP MACROS

--- 01TemplateMacros/02Process.py
+++ 01TemplateMacros/02Process.py
@@ -1,15 +1,9 @@
-# ---------------------------------------------------------------------------------------------------------------------- #
-#  =========================================== RUN:$ python3 02Process.py TEST ========================================= #
-# This macro will use peak/pedestal variables (PRE-PROCESS) and save the ADC wvf with the new baseline/polarity ...      #
-# Ideally we want to work in /pnfs/ciemat.es/data/neutrinos/FOLDER and so we mount the folder in our computer with:      #
-# $ sshfs USER@pcaeXYZ.ciemat.es:/pnfs/ciemat.es/data/neutrinos/FOLDER ../data  --> making sure empty data folder exists #
-# ---------------------------------------------------------------------------------------------------------------------- #
-
-import sys; sys.path.insert(0, '../'); from lib import *; print_header()
-try:               input_file = sys.argv[1]
-except IndexError: input_file = input("Please select input File: ")
-
+import sys; sys.path.insert(0, '../'); from lib import *
+user_input = initialize_macro("02Process")
+input_file = user_input["input_file"]
+debug = user_input["debug"]
 info = read_input_file(input_file)
+
 runs = []; channels = []
 runs = np.append(runs,info["CALIB_RUNS"])
 runs = np.append(runs,info["LIGHT_RUNS"])
@@ -26,12 +20,11 @@ for run, ch in product(runs.astype(int),channels.astype(int)):
     compute_ana_wvfs(my_runs,debug=False)
 
     insert_variable(my_runs,np.ones(len(channels)),"PChannel") # Change polarity!
-    compute_peak_variables(my_runs,key="ADC")                  # Compute new peak variables
+    compute_peak_variables(my_runs,label=str(info["ANA_LABEL"][0]), key="ADC")  # Compute new peak variables
     # compute_pedestal_variables(my_runs,key="ADC", buffer = 150, debug=False)  # Compute new ped variables
-    compute_pedestal_variables_sliding_window(my_runs,key="ADC", debug = False) # Compute new ped variables using sliding window
+    compute_pedestal_variables_sliding_window(my_runs, key="ADC", label=str(info["ANA_LABEL"][0]), debug = debug) # Compute new ped variables using sliding window
 
-    average_wvfs(my_runs,centering="NONE") # Compute average wvfs centering (choose from: "NONE", "PEAK", "THRESHOLD")
-    # average_wvfs(my_runs,centering="THRESHOLD") # Compute average wvfs centering (choose from: "NONE", "PEAK", "THRESHOLD")
+    average_wvfs(my_runs,label=str(info["ANA_LABEL"][0]), centering="PEAK") # Compute average wvfs centering (choose from: "NONE", "PEAK", "THRESHOLD")
 
     delete_keys(my_runs,["RawADC",'RawPeakAmp', 'RawPeakTime', 'RawPedSTD', 'RawPedMean', 'RawPedMax', 'RawPedMin', 'RawPedLim','RawPChannel']) # Delete branches to avoid overwritting
     save_proccesed_variables(my_runs,preset=str(info["SAVE_PRESET"][2]),info=info, force=True) # Try preset ANA
