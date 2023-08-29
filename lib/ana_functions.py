768cb5645ceb6f06e239b2597dda79695343fa39
mantheys <sergio.manthey@gmail.com>
2023-08-29 16:56:59 +0200

    CLEAN UP

--- lib/ana_functions.py
+++ lib/ana_functions.py
@@ -156,8 +156,8 @@ def get_units(my_runs, debug = False):
         aux_dic = {}
         for key in keys:
             if "Amp" in key or "Ped" in key or "ADC" in key: aux_dic[key] = "ADC"
-            elif "Time" in key or "Sampling" in key:         aux_dic[key] = "s"
-            elif "Charge" in key:                            aux_dic[key] = "pC"
+            elif "Time" in key or "Sampling" in key:         aux_dic[key] = "ticks"
+            elif "Charge" in key:                            aux_dic[key] = "ADC x ticks"
             else:                                            aux_dic[key] = "a.u."
             
         my_runs[run][ch]["UnitsDict"] = aux_dic
