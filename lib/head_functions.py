a2568be5093a5148a2426b4dfcfe644fc4ed561b
Laura Pérez-Molina <laurapermol@gmail.com>
2023-08-30 15:55:57 +0200

    questions everywhere

--- lib/head_functions.py
+++ lib/head_functions.py
@@ -55,10 +55,9 @@ def initialize_macro(macro, input_list=["input_file","debug"], default_dict={}, 
                         exit()
 
     user_input = select_input_file(user_input, debug=debug)
+    user_input["input_file"] = user_input["input_file"][0]
     if "cuts" in input_list: user_input = apply_cuts(user_input, debug=debug)
     user_input = update_user_input(user_input,input_list,debug=debug)
-    
-    user_input["input_file"] = user_input["input_file"][0]
     user_input["debug"] = user_input["debug"][0].lower() in ['true', '1', 't', 'y', 'yes']
     user_input = use_default_input(user_input, default_dict, debug=debug)
 
@@ -77,12 +76,16 @@ def update_user_input(user_input,new_input_list,debug=False):
     from .io_functions import check_key
 
     new_user_input = user_input.copy()
+    defaults = {"load_preset":"ANA","save_preset":"ANA","key":"AnaADC","variables":"AnaPeakAmp","runs":"1","channels":"0","debug":"y"}
+    flags = {"load_preset":"-l","save_preset":"-s","key":"-k","variables":"-v","runs":"-r","channels":"-c","debug":"-d"}
     for key_label in new_input_list:
         if check_key(user_input, key_label) == False:
-            new_user_input[key_label]= input("Please select %s (separated with commas): "%key_label).split(",")
-        else:
+
+            q = [ inquirer.Text(key_label, message="Please select %s [flag: %s]"%(key_label,flags[key_label]), default=defaults[key_label]) ]
+            new_user_input[key_label] =  [inquirer.prompt(q)[key_label]]
+            # new_user_input[key_label] = input("Please select %s (separated with commas): "%key_label).split(",")
+        else: pass
             # if debug: print("Using %s from user input"%key_label)
-            pass
     
     return new_user_input
 
@@ -99,7 +102,7 @@ def select_input_file(user_input, debug=False):
     new_user_input = user_input.copy()
     if check_key(user_input, "input_file") == False:
         file_names = [file_name.replace(".txt", "") for file_name in os.listdir('../input')]
-        q = [ inquirer.List("input_file", message="Please select input file", choices=file_names, default="TUTORIAL") ]
+        q = [ inquirer.List("input_file", message="Please select input file [flag: -i]", choices=file_names, default="TUTORIAL") ]
         new_user_input["input_file"] = [inquirer.prompt(q)["input_file"]]
     if debug: print_colored("Using input file %s"%new_user_input["input_file"][0],"INFO")
     return new_user_input
@@ -129,15 +132,6 @@ def use_default_input(user_input, default_dict, debug=False):
             new_user_input[default_key] = runs
             if new_user_input["debug"]:print_colored("No runs provided. Using all %s from input file. %s"%(default_key,runs),"WARNING")
 
-    # if check_key(user_input, "channels") == False:
-    #     channels = []
-    #     for key in default_dict["channels"]:
-    #         if check_key(info, key):
-    #             for channel in info[key]:
-    #                 if channel not in channels:
-    #                     channels.append(channel)
-    #     new_user_input["channels"] = channels
-    #     if new_user_input["debug"]: print_colored("No channels provided. Using all channels from input file. %s"%channels,"WARNING")
     return new_user_input
 
 def print_macro_info(macro, debug=False):
@@ -167,8 +161,7 @@ def apply_cuts(user_input, debug=False):
     new_user_input = user_input.copy()
     if check_key(user_input, "cuts") == False:
         cuts_choices = ["cut_df","cut_lin_rel","cut_peak_finder"]
-        # cuts_choices = ["cut_min_max","cut_ped_std","cut_lin_rel","cut_peak_finder","cut_min_max_sim"]
-        q = [ inquirer.Checkbox("cuts", message="Please select input file", choices=cuts_choices) ]
+        q = [ inquirer.Checkbox("cuts", message="Please select the cuts you want to apply", choices=cuts_choices) ]
         my_cuts = [inquirer.prompt(q)["cuts"]][0]
         cut_dict = dict.fromkeys(cuts_choices)
         for cut in cuts_choices:
@@ -180,17 +173,6 @@ def apply_cuts(user_input, debug=False):
                     value = [ inquirer.Text("value", message="Please select value for applying **%s**"%cut, default="1") ]
                     inclusive = [ inquirer.Text("inclusive", message="Please select inclusive for applying **%s**"%cut, default="False") ]
                     cut_dict[cut] = [True, inquirer.prompt(channels)["channels"].split(','),inquirer.prompt(key)["key"].split(','), inquirer.prompt(logic)["logic"].split(','), inquirer.prompt(value)["value"].split(','), inquirer.prompt(inclusive)["inclusive"].split(',')]
-                # if cut == "cut_min_max":
-                #     key = [ inquirer.Text("key", message="Please select key for applying **%s**"%cut, default="PedSTD") ]
-                #     lim = [ inquirer.Text("lim", message="Please select limits for applying **%s**"%cut, default="-1,7.5") ]
-                #     chann2cut = [ inquirer.Text("ch2cut", message="Please select the CHANNELS to be cut", default="0,1") ]
-                #     apply_all = [ inquirer.Text("apply",  message="Do you what the cuts to be applied to all the LOADED channels?", default="y") ]
-                #     cut_dict[cut] = [True, inquirer.prompt(key)["key"], list(map(float, inquirer.prompt(lim)["lim"].split(','))), list(map(int, inquirer.prompt(chann2cut)["ch2cut"].split(','))), inquirer.prompt(apply_all)["apply"].lower() in ['true', '1', 't', 'y', 'yes']]
-                # if cut == "cut_ped_std":
-                #     n_std = [ inquirer.Text("n_std", message="Please select number of std for applying **%s**"%cut, default="2") ]
-                #     chann2cut = [ inquirer.Text("ch2cut", message="Please select the CHANNELS to be cut", default="0,1") ]
-                #     apply_all = [ inquirer.Text("apply",  message="Do you what the cuts to be applied to all the LOADED channels?", default="y") ]
-                #     cut_dict[cut] = [True, float(inquirer.prompt(n_std)["n_std"]),list(map(int, inquirer.prompt(chann2cut)["ch2cut"].split(','))), inquirer.prompt(apply_all)["apply"].lower() in ['true', '1', 't', 'y', 'yes']]
                 if cut == "cut_lin_rel":
                     key = [ inquirer.Text("key", message="Please select 2 keys for applying **%s**"%cut, default="AnaPeakAmp,AnaChargeAveRange") ]
                     compare = [ inquirer.Text("compare", message="NONE, RUNS, CHANNELS to decide the histogram to use", default="NONE") ]
@@ -198,10 +180,6 @@ def apply_cuts(user_input, debug=False):
                 if cut == "cut_peak_finder":
                     n_peaks = [ inquirer.Text("n_peaks", message="Please select number of peaks for applying **%s**"%cut, default="1") ]
                     cut_dict[cut] = [True, inquirer.prompt(n_peaks)["n_peaks"]]
-                # if cut == "cut_min_max_sim":
-                #     key = [ inquirer.Text("key", message="Please select key for applying **%s**"%cut, default="PeakAmp") ]
-                #     lim = [ inquirer.Text("lim", message="Please select limits for applying **%s**"%cut, default="20,50") ]
-                #     cut_dict[cut] = [True, inquirer.prompt(key)["key"].split(','), inquirer.prompt(lim)["lim"]]
 
             else: cut_dict[cut] = [False]
 
