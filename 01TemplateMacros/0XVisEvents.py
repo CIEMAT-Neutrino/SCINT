a9a3b01682c24bdf22e5904b3f0671a1a95d9d2a
mantheys <sergio.manthey@gmail.com>
2023-08-31 14:14:31 +0200

    CLEAN MACRO

--- 01TemplateMacros/0XVisEvents.py
+++ 01TemplateMacros/0XVisEvents.py
@@ -1,6 +1,6 @@
 import sys; sys.path.insert(0, '../'); from lib import *
-default_dict = {}
-user_input = initialize_macro("00Raw2Np",["input_file","load_preset","key","runs","channels","cuts","debug"],default_dict=default_dict, debug=True)
+user_input = initialize_macro("0XVisEvents",["input_file","load_preset","key","runs","channels","cuts","debug"],default_dict={}, debug=True)
+info = read_input_file(user_input["input_file"], debug=user_input["debug"])
 
 OPT  = {
     "MICRO_SEC":   True,                 # Time in microseconds (True/False)
@@ -15,8 +15,7 @@ OPT  = {
     "LEGEND":      False                 # Shows plot legend (True/False)
 }
 
-# 0XVisEvent
-info = read_input_file(user_input["input_file"], debug=user_input["debug"])
+### 0XVisEvent
 my_runs = load_npy(np.asarray(user_input["runs"]).astype(int),np.asarray(user_input["channels"]).astype(int),preset=user_input["load_preset"][0],info=info,compressed=True) # preset could be RAW or ANA
 label, my_runs = cut_selector(my_runs, user_input)
 vis_npy(my_runs, user_input["key"],OPT=OPT) # Remember to change key accordingly (ADC or RawADC)
