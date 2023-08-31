8013ecbd839e6a132b200f3da208760d30f797c9
Laura Pérez-Molina <laurapermol@gmail.com>
2023-08-31 10:26:19 +0200

    tenemos un toc

--- 01TemplateMacros/0XVisEvents.py
+++ 01TemplateMacros/0XVisEvents.py
@@ -3,7 +3,7 @@ default_dict = {}
 user_input = initialize_macro("00Raw2Np",["input_file","load_preset","key","runs","channels","cuts","debug"],default_dict=default_dict, debug=True)
 
 OPT  = {
-    "MICRO_SEC":   True,
+    "MICRO_SEC":   True,                 # Time in microseconds (True/False)
     "NORM":        False,                # Runs can be displayed normalised (True/False)
     "LOGY":        False,                # Runs can be displayed in logy (True/False)
     "SHOW_AVE":    "AveWvf",             # If computed, vis will show average (AveWvf,AveWvfSPE,etc.)
