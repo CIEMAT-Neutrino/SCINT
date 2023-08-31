b85f2e352875cf9bd4ff4cd6ee9031c88a721640
mantheys <sergio.manthey@gmail.com>
2023-08-31 14:14:49 +0200

    CLEAN MACRO

--- 01TemplateMacros/05Scintillation.py
+++ 01TemplateMacros/05Scintillation.py
@@ -1,6 +1,6 @@
 import sys; sys.path.insert(0, '../'); from lib import *
 default_dict = {"runs":["ALPHA_RUNS"],"channels":["CHAN_TOTAL"],"key":["ANA_KEY"],"variables":["TYPE"]}
-user_input = initialize_macro("04Calibration",["input_file","debug"],default_dict=default_dict, debug=True)
+user_input = initialize_macro("05Calibration",["input_file","debug"],default_dict=default_dict, debug=True)
 info = read_input_file(user_input["input_file"], debug=user_input["debug"])
 
 int_key = user_input["variables"]
@@ -12,23 +12,11 @@ OPT = {
     "SCINT_FIT":True,
     "LEGEND":False,     # Shows plot legend (True/False)
     "SHOW": True
-    }
+}
 
-
-## Visualize average waveforms by runs/channels ##
-# my_runs = load_npy(runs.astype(int),channels.astype(int),branch_list=["Label","Sampling","AveWvf"],info=info,compressed=True) #Remember to LOAD your wvf
-# my_runs = load_npy([25],[0],branch_list=["Label","Sampling","AveWvf"],info=info,compressed=True) #Remember to LOAD your wvf
-# vis_compare_wvf(my_runs, ["AveWvf"], compare="RUNS", OPT=OPT)
-#################################################
-
-# for run, ch in product(np.asarray(user_input["runs"]).astype(int),np.asarray(user_input["channels"]).astype(int)):
 my_runs = load_npy(np.asarray(user_input["runs"]).astype(int),np.asarray(user_input["channels"]).astype(int), info, preset=info["LOAD_PRESET"][4], compressed=True, debug=user_input["debug"])
 print_keys(my_runs)
 
-## Integrated charge (scintillation runs) ##
-# print("Run ", run, "Channel ", ch)
-# cut_min_max(my_runs, ["PedSTD","PeakAmp","PeakTime"], {"PedSTD": [0,4.5],"PeakAmp": [60,650],"PeakTime": [3.7e-6,4e-6]}, chs_cut=[0,1,3,4], apply_all_chs=True)
-# cut_min_max(my_runs, ["PedSTD","PeakAmp","PeakTime"], {"PedSTD": [0,9],"PeakAmp": [1400,4500],"PeakTime": [3.7e-6,4e-6]}, chs_cut=[5], apply_all_chs=True)
 popt_ch = []; pcov_ch = []; perr_ch = []; popt_nevt = []; pcov_nevt = []; perr_nevt = []
 popt, pcov, perr = charge_fit(my_runs, [user_input["key"][0].split("ADC")[0]+int_key[0]], OPT); popt_ch.append(popt); pcov_ch.append(pcov); perr_ch.append(perr)
 #################################################
