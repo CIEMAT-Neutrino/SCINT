9ce7bdb50d21b07c6fe633dcbfbcf5b12ad2e733
mantheys <sergio.manthey@gmail.com>
2023-09-11 11:03:24 +0200

    REFORMAT MACROS

--- 01TemplateMacros/03Integration.py
+++ 01TemplateMacros/03Integration.py
@@ -1,5 +1,5 @@
 import sys; sys.path.insert(0, '../'); from lib import *
-default_dict = {"runs":["CALIB_RUNS","LIGHT_RUNS","ALPHA_RUNS","MUON_RUNS","NOISE_RUNS"],"channels":["CHAN_TOTAL"]}
+default_dict = {"runs":["CALIB_RUNS","LIGHT_RUNS","ALPHA_RUNS","MUON_RUNS","NOISE_RUNS"],"channels":["CHAN_TOTAL"],"key":["ANA_KEY"]}
 user_input = initialize_macro("03Integration",["input_file","key","debug"],default_dict=default_dict, debug=True)
 info = read_input_file(user_input["input_file"], debug=user_input["debug"])
 
