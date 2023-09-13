9e076b8d9694ce40e1c4873982a2eb7ea0e7c4fc
mantheys <sergio.manthey@gmail.com>
2023-09-13 19:02:21 +0200

    ADD DEBUG

--- 01TemplateMacros/0YVisHist1D.py
+++ 01TemplateMacros/0YVisHist1D.py
@@ -7,4 +7,4 @@ OPT = opt_selector(debug=user_input["debug"])
 ### 0YVisHist1D
 my_runs = load_npy(np.asarray(user_input["runs"]).astype(int), np.asarray(user_input["channels"]).astype(int), preset="EVA", info=info, compressed=True) # preset could be RAW or ANA
 label, my_runs = cut_selector(my_runs, user_input, debug=user_input["debug"])
-vis_var_hist(my_runs, user_input["variables"], percentile = [0.1, 99.9], OPT = OPT, select_range=False)
+vis_var_hist(my_runs, user_input["variables"], percentile = [0.1, 99.9], OPT = OPT, select_range=False, debug=user_input["debug"])
