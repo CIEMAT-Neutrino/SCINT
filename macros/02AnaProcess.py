a5f5075ae11a226117c942a1fbd2ccafcf01f4e3
mantheys <sergio.manthey@gmail.com>
2023-10-03 11:51:54 +0200

    IMPROVE COUSTOM FEATURES. INCLUDE RawADC AS ANA_KEY

--- macros/02AnaProcess.py
+++ macros/02AnaProcess.py
@@ -1,5 +1,5 @@
 import sys; sys.path.insert(0, '../'); from lib import *
-default_dict = {"runs":["CALIB_RUNS","LIGHT_RUNS","ALPHA_RUNS","MUONS_RUNS","NOISE_RUNS"],"channels":["CHAN_TOTAL"],"key":["ANA_KEY"]}
+default_dict = {"runs":["CALIB_RUNS","LIGHT_RUNS","ALPHA_RUNS","MUONS_RUNS","NOISE_RUNS"],"channels":["CHAN_TOTAL"]}
 user_input = initialize_macro("02AnaProcess",["input_file","debug"],default_dict=default_dict, debug=True)
 info = read_input_file(user_input["input_file"][0], debug=user_input["debug"])
 
@@ -9,8 +9,8 @@ for run, ch in product(np.asarray(user_input["runs"]).astype(int),np.asarray(use
     compute_ana_wvfs(my_runs,debug=False)
     delete_keys(my_runs,['RawADC','RawPeakAmp', 'RawPeakTime', 'RawPedSTD', 'RawPedMean', 'RawPedMax', 'RawPedMin', 'RawPedLim']) # Delete branches to avoid overwritting
 
-    compute_peak_variables(my_runs, label="Ana", key=user_input["key"][0], debug=user_input["debug"])     # Compute new peak variables
-    compute_pedestal_variables(my_runs, label="Ana", key=user_input["key"][0], debug=user_input["debug"]) # Compute new ped variables using sliding window
+    compute_peak_variables(my_runs, label="Ana", key="AnaADC", debug=user_input["debug"])     # Compute new peak variables
+    compute_pedestal_variables(my_runs, label="Ana", key="AnaADC", debug=user_input["debug"]) # Compute new ped variables using sliding window
 
     save_proccesed_variables(my_runs,preset=str(info["SAVE_PRESET"][2]),info=info, force=True)
     del my_runs
