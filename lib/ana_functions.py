e1910311d15e0018a19e3ce1d99fec1c13a39d1c
mantheys <sergio.manthey@gmail.com>
2023-10-09 17:54:04 +0200

    ADD PRETRIGGER ADC UNITS LABEL

--- lib/ana_functions.py
+++ lib/ana_functions.py
@@ -287,11 +287,11 @@ def get_units(my_runs, debug = False):
         keys = my_runs[run][ch].keys()
         aux_dic = {}
         for key in keys:
-            if "Amp" in key or "Ped" in key or "ADC" in key: aux_dic[key] = "ADC"
-            elif "Time" in key or "Sampling" in key:         aux_dic[key] = "ticks"
-            elif "Charge" in key and "Ana" in key:           aux_dic[key] = "ADC x ticks"
-            elif "Charge" in key and "Gauss" in key:         aux_dic[key] = "PE"
-            else:                                            aux_dic[key] = "a.u."
+            if "Amp" in key or "Ped" in key or "ADC" in key or "PreTrigger" in key: aux_dic[key] = "ADC"
+            elif "Time" in key or "Sampling" in key:                                aux_dic[key] = "ticks"
+            elif "Charge" in key and "Ana" in key:                                  aux_dic[key] = "ADC x ticks"
+            elif "Charge" in key and "Gauss" in key:                                aux_dic[key] = "PE"
+            else:                                                                   aux_dic[key] = "a.u."
             
         my_runs[run][ch]["UnitsDict"] = aux_dic
 
