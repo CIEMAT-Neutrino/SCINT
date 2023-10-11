0a525289d4002e64c3c51b10ee28667f19335006
mantheys <sergio.manthey@gmail.com>
2023-10-11 16:02:13 +0200

    CLEANUP

--- macros/0VVisPersistance.py
+++ macros/0VVisPersistance.py
@@ -1,6 +1,7 @@
 import sys; sys.path.insert(0, '../'); from lib import *
-user_input = initialize_macro("0VVisPersistance",["input_file","load_preset","variables","runs","channels","debug"],default_dict={}, debug=True)
+user_input = initialize_macro("0VVisPersistance",["input_file","load_preset","runs","channels","filter","debug"],default_dict={}, debug=True)
 info = read_input_file(user_input["input_file"][0], debug=user_input["debug"])
 ### 0VVisPersistance
 my_runs = load_npy(np.asarray(user_input["runs"]).astype(int),np.asarray(user_input["channels"]).astype(int), info, preset=user_input["load_preset"][0], compressed=True, debug=user_input["debug"])
-vis_persistence(my_runs)
+label, my_runs = cut_selector(my_runs, user_input)
+vis_persistence(my_runs, info, debug=user_input["debug"])
