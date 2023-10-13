47776f059c107605f8ad7651e4489fc4c9e119b6
mantheys <sergio.manthey@gmail.com>
2023-10-13 17:36:49 +0200

    LOAD PRESET DETERMINES BEHAVIOUR OF ADC COMPUTATION

--- macros/01PreProcess.py
+++ macros/01PreProcess.py
@@ -1,19 +1,24 @@
 import sys; sys.path.insert(0, '../'); from lib import *
-default_dict = {"runs":["CALIB_RUNS","LIGHT_RUNS","ALPHA_RUNS","MUONS_RUNS","NOISE_RUNS"],"channels":["CHAN_TOTAL"],"key":["ANA_KEY"]}
+default_dict = {"runs":["CALIB_RUNS","LIGHT_RUNS","ALPHA_RUNS","MUONS_RUNS","NOISE_RUNS"],"channels":["CHAN_TOTAL"]}
 user_input = initialize_macro("01PreProcess",["input_file","debug"],default_dict=default_dict, debug=True)
 info = read_input_file(user_input["input_file"][0], debug=user_input["debug"])
 ### 01PreProcess
 for run, ch in product(np.asarray(user_input["runs"]).astype(int),np.asarray(user_input["channels"]).astype(int)):
     ### Load
     my_runs = load_npy([run],[ch], info, preset=info["LOAD_PRESET"][1], compressed=True, debug=user_input["debug"])
+    ### Process
+    if info["LOAD_PRESET"][1] == "ANA":
+        if check_key(my_runs[my_runs["NRun"][0]][my_runs["NChannel"][0]],"AnaADC") == False: compute_ana_wvfs(my_runs,info=info,debug=False)
+        delete_keys(my_runs,['RawADC','RawPeakAmp','RawPeakTime','RawPedSTD','RawPedMean','RawPedMax','RawPedMin','RawPedLim'])
     ### Compute
-    compute_peak_variables(my_runs, key=user_input["key"][0], label="", debug=user_input["debug"])
-    compute_pedestal_variables(my_runs, key=user_input["key"][0], label="", debug=user_input["debug"]) # Checking the best window in the pretrigger
-    average_wvfs(my_runs, info=info, key=user_input["key"][0], centering="NONE", debug=user_input["debug"])
-    average_wvfs(my_runs, info=info, key=user_input["key"][0], centering="PEAK", debug=user_input["debug"])
+    key, label = get_wvf_label(my_runs, "", "", debug=user_input["debug"])
+    compute_peak_variables(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
+    compute_pedestal_variables(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
+    average_wvfs(my_runs, info=info, key=key, label=label, centering="NONE", debug=user_input["debug"])
+    average_wvfs(my_runs, info=info, key=key, label=label, centering="PEAK", debug=user_input["debug"])
+    # compute_peak_RMS(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
     ### Remove branches to exclude from saving
-    delete_keys(my_runs,[user_input["key"][0],"TimeStamp","Sampling"]) # Delete previous peak and pedestal variables
-    save_proccesed_variables(my_runs, info, preset=info["SAVE_PRESET"][1], force=True, debug=user_input["debug"])
+    save_proccesed_variables(my_runs,preset=str(info["SAVE_PRESET"][2]),info=info, force=True)
     ### Free memory
     del my_runs
     gc.collect()
--- macros/02AnaProcess.py
+++ macros/02AnaProcess.py
@@ -7,16 +7,17 @@ for run, ch in product(np.asarray(user_input["runs"]).astype(int),np.asarray(use
     ### Load
     my_runs = load_npy([run],[ch], info, preset=info["LOAD_PRESET"][2], compressed=True, debug=user_input["debug"])
     ### Process
-    compute_ana_wvfs(my_runs,info,debug=False)
-    delete_keys(my_runs,['RawADC','RawPeakAmp','RawPeakTime','RawPedSTD','RawPedMean','RawPedMax','RawPedMin','RawPedLim'])
+    if info["LOAD_PRESET"][2] == "ANA":
+        if check_key(my_runs[my_runs["NRun"][0]][my_runs["NChannel"][0]],"AnaADC") == False: compute_ana_wvfs(my_runs,info=info,debug=False)
+        delete_keys(my_runs,['RawADC','RawPeakAmp','RawPeakTime','RawPedSTD','RawPedMean','RawPedMax','RawPedMin','RawPedLim'])
     ### Compute
-    compute_peak_variables(my_runs, label="Ana", key="AnaADC", debug=user_input["debug"])
-    compute_pedestal_variables(my_runs, label="Ana", key="AnaADC", debug=user_input["debug"])
-    average_wvfs(my_runs, info=info, label="Ana", key="AnaADC", centering="NONE", debug=user_input["debug"])
-    average_wvfs(my_runs, info=info, label="Ana", key="AnaADC", centering="PEAK", debug=user_input["debug"])
-    compute_peak_RMS(my_runs, info=info, label="Ana", key="AnaADC", debug=user_input["debug"])
+    key, label = get_wvf_label(my_runs, "", "", debug=user_input["debug"])
+    compute_peak_variables(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
+    compute_pedestal_variables(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
+    average_wvfs(my_runs, info=info, key=key, label=label, centering="NONE", debug=user_input["debug"])
+    average_wvfs(my_runs, info=info, key=key, label=label, centering="PEAK", debug=user_input["debug"])
+    compute_peak_RMS(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
     ### Remove branches to exclude from saving
-    # delete_keys(my_runs,["TimeStamp","Sampling"])
     save_proccesed_variables(my_runs,preset=str(info["SAVE_PRESET"][2]),info=info, force=True)
     ### Free memory
     del my_runs
