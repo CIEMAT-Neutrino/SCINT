0a7d75c9fb9f02605c60924a10a05e4a4d4f10be
mantheys <sergio.manthey@gmail.com>
2023-10-14 02:16:48 +0200

    CLEANUP MACROS

--- macros/01PreProcess.py
+++ macros/01PreProcess.py
@@ -1,7 +1,6 @@
 import sys; sys.path.insert(0, '../'); from lib import *
 default_dict = {"runs":["CALIB_RUNS","LIGHT_RUNS","ALPHA_RUNS","MUONS_RUNS","NOISE_RUNS"],"channels":["CHAN_TOTAL"]}
 user_input, info = initialize_macro("01PreProcess",["input_file","debug"],default_dict=default_dict, debug=True)
-# info = read_input_file(user_input["input_file"][0], debug=user_input["debug"])
 ### 01PreProcess
 for run, ch in product(np.asarray(user_input["runs"]).astype(int),np.asarray(user_input["channels"]).astype(int)):
     ### Load
@@ -10,13 +9,14 @@ for run, ch in product(np.asarray(user_input["runs"]).astype(int),np.asarray(use
     if info["LOAD_PRESET"][1] == "ANA":
         if check_key(my_runs[my_runs["NRun"][0]][my_runs["NChannel"][0]],"AnaADC") == False: compute_ana_wvfs(my_runs,info=info,debug=user_input["debug"])
         delete_keys(my_runs,['RawADC','RawPeakAmp','RawPeakTime','RawPedSTD','RawPedMean','RawPedMax','RawPedMin','RawPedLim'])
-    ### Compute
+    ### Check if the run is empty
     key, label = get_wvf_label(my_runs, "", "", debug=user_input["debug"])
+    if key == "" and label == "": continue
+    ### Compute
     compute_peak_variables(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
     compute_pedestal_variables(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
     average_wvfs(my_runs, info=info, key=key, label=label, centering="NONE", debug=user_input["debug"])
     average_wvfs(my_runs, info=info, key=key, label=label, centering="PEAK", debug=user_input["debug"])
-    # compute_peak_RMS(my_runs, info=info, key=key, label=label, debug=user_input["debug"])
     ### Remove branches to exclude from saving
     save_proccesed_variables(my_runs,preset=str(info["SAVE_PRESET"][2]),info=info, force=True)
     ### Free memory
--- macros/10Ana2Root.py
+++ macros/10Ana2Root.py
@@ -1,11 +1,9 @@
 import sys; sys.path.insert(0, '../'); from lib import *
 default_dict = {"runs":["CALIB_RUNS","LIGHT_RUNS","ALPHA_RUNS","MUON_RUNS","NOISE_RUNS"],"channels":["CHAN_TOTAL"]}
 user_input, info = initialize_macro("10Ana2Root",["input_file","variables","debug"],default_dict=default_dict, debug=True)
-# info = read_input_file(user_input["input_file"][0], debug=user_input["debug"])
-
 ### 10Ana2Root
-my_runs = load_npy(np.asarray(user_input["runs"]).astype(int), np.asarray(user_input["channels"]).astype(int), preset="EVA", info=info, compressed=True) # preset could be RAW or ANA
-delete_keys(my_runs,["Label","NBinsWvf","TimeStamp","Sampling","PeakAmp","PedLim","ChargeRangeDict","AnaChargeRangeDict"])
+my_runs = load_npy(np.asarray(user_input["runs"]).astype(int), np.asarray(user_input["channels"]).astype(int), preset="EVA", info=info, compressed=True, debug=user_input["debug"])
+delete_keys(my_runs, ["Label","NBinsWvf","TimeStamp","Sampling","PeakAmp","PedLim","ChargeRangeDict","AnaChargeRangeDict"])
 root_df = npy2root(my_runs[user_input["runs"][0]][user_input["channels"][0]], debug=user_input["debug"])
 root_df.Display().Print()
 
