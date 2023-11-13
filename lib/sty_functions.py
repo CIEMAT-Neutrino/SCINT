6806c8055e4da31bc181a1f47308e3a4a9290e7f
LauPM <laurapermol@gmail.com>
2023-11-13 14:13:09 +0100

    bug in styles

--- lib/sty_functions.py
+++ lib/sty_functions.py
@@ -1,8 +1,11 @@
 
 styles = ["CIEMAT_style"]
 import importlib.util
-if importlib.util.find_spec("dunestyle.matplotlib") is not None: styles.append("DUNE_style")
-if importlib.util.find_spec("mplhep") is not None:               styles.append("HEP_style")
+try: importlib.util.find_spec("dunestyle.matplotlib"); styles.append("DUNE_style")
+except: pass
+try: importlib.util.find_spec("mplhep"); styles.append("HEP_style")
+except: pass
+
 for style in styles: print(" \t * You can change your plotting style with "+ '\033[1m' + "OPT[STYLE]=" + style + '\033[0m' +" !")
 
 #  Import from other libraries
