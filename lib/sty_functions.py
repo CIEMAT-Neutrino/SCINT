0751ef7ad20e731dff2f73865370eb92d07c2050
LauPM <laurapermol@gmail.com>
2023-11-20 12:11:38 +0100

    default python style

--- lib/sty_functions.py
+++ lib/sty_functions.py
@@ -13,6 +13,7 @@ from .io_functions  import check_key
 from .fig_config import (figure_features, add_grid); 
 def style_selector(OPT):
     if check_key(OPT,"STYLE") == False: OPT["STYLE"] = "CIEMAT_style"
+    if OPT["STYLE"] == "None": pass
     if OPT["STYLE"] == "CIEMAT_style": figure_features()
     if OPT["STYLE"] == "DUNE_style":   import dunestyle.matplotlib as dune
     if OPT["STYLE"] == "HEP_style":    import mplhep as hep
