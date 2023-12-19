741ec8736b25ea98cec6b57193d536b45ab3047e
mantheys <sergio.manthey@ciemat.es>
2023-12-20 00:31:11 +0100

    CLEANUP

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,12 +1,13 @@
+from .ana_functions  import *
+from .cal_functions  import *
+from .cut_functions  import *
+from .dec_functions  import *
+from .fig_config     import (add_grid, figure_features)  # <--- import customized functions
+from .fit_functions  import *
 from .head_functions import *
 from .io_functions   import *
-from .ana_functions  import *
-from .wvf_functions  import *
-from .cal_functions  import *
-from .vis_functions  import *
 from .ply_functions  import *
-from .cut_functions  import *
-from .fit_functions  import *
-from .dec_functions  import *
 from .sty_functions  import (style_selector)
-from .fig_config     import (add_grid, figure_features)  # <--- import customized functions
+from .sum_functions  import*
+from .vis_functions  import *
+from .wvf_functions  import *
