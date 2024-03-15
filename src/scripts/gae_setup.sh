3135e7d85d8109884445f627e263bf8f43d02f93
mantheys <sergio.manthey@ciemat.es>
2024-03-15 18:02:41 +0100

    MOVE SCRIPTS

---
+++ src/scripts/gae_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# FILEPATH: /afs/ciemat.es/user/m/manthey/Code/SCINT/scripts/gae_setup.sh
+
+source .venv/bin/activate
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
