6905a6d908bcac76683a445d244697c82807df0c
mantheys <sergio.manthey@ciemat.es>
2024-03-15 13:59:46 +0100

    IMPORT FORMAT_COUSTOM_PLOTLY

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,7 +5,7 @@ from .cal_functions     import *
 from .group_functions   import*
 from .cut_functions     import *
 from .dec_functions     import *
-from .fig_config        import (add_grid, figure_features)  # <--- import customized functions
+from .fig_config        import (add_grid, figure_features, format_coustom_plotly)  # <--- import customized functions
 from .fit_functions     import *
 from .head_functions    import *
 from .io_functions      import *
