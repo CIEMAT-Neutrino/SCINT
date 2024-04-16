36b98f6501edcf160ba4e1a4ba44b7cec593b22e
LauPM <laurapermol@gmail.com>
2024-04-16 14:53:51 +0200

    clean

--- TMP/joython/macros/run_full_chain copy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Pedestal & channel parallelized computation, $1 is the week as string argument to analyze
-
-( time cat RUN.txt | ~/parallel --joblog out.txt -j8 "python3 01_Process_Pedestal_and_peak.py -w $1 -r" ) &&
-( time cat RUN.txt | ~/parallel --joblog out.txt -j8 "python3 03_Raw_Charges.py -w $1 -r" )
