fda3dd0abaca3af54f438acd0c382915b7b0a4fc
mantheys <sergio.manthey@ciemat.es>
2024-07-03 14:30:32 +0200

    COMPUTE NOISE AVERAGE WVF FROM CALIB

--- src/macros/11AverageSPE.py
+++ src/macros/11AverageSPE.py
@@ -15,14 +15,19 @@ for run, ch, variable in product(np.asarray(user_input["runs"]).astype(str),np.a
     key, label = get_wvf_label(my_runs, "", "", debug=user_input["debug"])
     if key == "" and label == "": pass
     ### Compute
+    branch_list = []
     user_input["filter"]["cut_df"][0] = True
-    user_input["filter"]["cut_df"][1] = [[[ch],variable,"between",(float(my_runs[run][ch]["AnaMinChargeSPE"]),float(my_runs[run][ch]["AnaMaxChargeSPE"])),False]]
-
-    print(user_input)
-    cut_label, my_runs = cut_selector(my_runs, user_input)
-    average_wvfs(my_runs, info, key=key, label=label, cut_label="SPE", centering="NONE", debug=user_input["debug"])
-    compute_peak_variables(my_runs, info=info, key="AnaAveWvfSPE", label="AnaAveSPE", debug=user_input["debug"])
+    for ref in ["Noise","SPE"]:
+        user_input["filter"]["cut_df"][1] = [[[ch],variable,"between",(float(my_runs[run][ch][f"AnaMinCharge{ref}"]),float(my_runs[run][ch][f"AnaMaxCharge{ref}"])),False]]
+        cut_label, my_runs = cut_selector(my_runs, user_input)
+        average_wvfs(my_runs, info, key=key, label=label, cut_label=ref, centering="NONE", debug=user_input["debug"])
+        compute_peak_variables(my_runs, info=info, key=f"AnaAveWvf{ref}", label=f"AnaAve{ref}", debug=user_input["debug"])
+        branch_list.append(f"AnaAveWvf{ref}")
+        branch_list.append(f"AnaAve{ref}PeakAmp")
+        branch_list.append(f"AnaAve{ref}PeakTime")
+    # my_runs[run][ch][f"AnaAveWvfClean"] = my_runs[run][ch][f"AnaAveWvfSPE"] - my_runs[run][ch][f"AnaAveWvfNoise"]
+    # branch_list.append(f"AnaAveWvfClean")
     ### Remove branches to exclude from saving
-    save_proccesed_variables(my_runs, preset=None, branch_list=["AnaAveWvfSPE","AnaAveSPEPeakAmp","AnaAveSPEPeakTime"], info=info, force=True, debug=user_input["debug"])
+    save_proccesed_variables(my_runs, preset=None, branch_list=branch_list, info=info, force=True, debug=user_input["debug"])
     del my_runs
     gc.collect()
